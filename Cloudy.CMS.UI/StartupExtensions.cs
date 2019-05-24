1c10e8f2484505f5e82c58c08e36fe2534e47b29
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-05-24 15:25:09 +0200

    AddCMSUI startup extension

---
+++ Cloudy.CMS.UI/StartupExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Poetry;
+using Poetry.AspNetCore.DependencyInjectionSupport;
+using Cloudy.CMS;
+using Cloudy.CMS.DocumentSupport;
+using Cloudy.CMS.ContentControllerSupport;
+using Cloudy.CMS.ContentSupport;
+using Cloudy.CMS.ContentSupport.RepositorySupport;
+using Cloudy.CMS.ContentSupport.Serialization;
+using Cloudy.CMS.ContentTypeSupport;
+using Cloudy.CMS.ContentTypeSupport.PropertyMappingSupport;
+using Cloudy.CMS.Core;
+using Cloudy.CMS.Core.ContentSupport.RepositorySupport;
+using Cloudy.CMS.Mvc.Routing;
+using Cloudy.CMS.Reflection;
+using Cloudy.CMS.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Cloudy.CMS.UI
+{
+    public static class StartupExtensions
+    {
+        public static void AddCMSUI(this PoetryConfigurator poetryConfigurator)
+        {
+            poetryConfigurator.AddComponent<CloudyCMSUIComponent>();
+        }
+    }
+}
