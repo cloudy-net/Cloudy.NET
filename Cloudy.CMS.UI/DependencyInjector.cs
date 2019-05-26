7da16a4fd5b7120b418c90f31ab41f6da40e7230
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-05-27 01:50:05 +0200

    Update Poetry.UI, set portal window title and favicon

---
+++ Cloudy.CMS.UI/DependencyInjector.cs
@@ -0,0 +1,17 @@
+﻿using Cloudy.CMS.UI.PortalSupport;
+using Poetry.DependencyInjectionSupport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudy.CMS.UI
+{
+    public class DependencyInjector : IDependencyInjector
+    {
+        public void InjectDependencies(IContainer container)
+        {
+            container.RegisterSingleton<Poetry.UI.PortalSupport.ITitleProvider, TitleProvider>();
+            container.RegisterSingleton<Poetry.UI.PortalSupport.IFaviconProvider, FaviconProvider>();
+        }
+    }
+}
---
+++ Cloudy.CMS.UI/PortalSupport/TitleProvider.cs
@@ -0,0 +1,14 @@
+﻿using Poetry.UI.PortalSupport;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Cloudy.CMS.UI.PortalSupport
+{
+    public class TitleProvider : ITitleProvider
+    {
+        static string Version { get; } = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        public string Title => $"Cloudy CMS v{Version}";
+    }
+}
