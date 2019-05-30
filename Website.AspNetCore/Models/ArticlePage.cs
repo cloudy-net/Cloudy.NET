6570a576e17d0b58b270309f5c7bc4791699188b
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-05-30 10:20:45 +0200

    Add ArticlePage to example project

---
+++ Website.AspNetCore/Models/ArticlePage.cs
@@ -0,0 +1,19 @@
+﻿using Cloudy.CMS.ContentSupport;
+using Cloudy.CMS.ContentTypeSupport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.AspNetCore.Models
+{
+    [ContentType("43b1eea2-9b7f-478e-8aca-f3cee8ce3f5a")]
+    public class ArticlePage : IContent, IRoutable, IHierarchical, INameable
+    {
+        public string Id { get; set; }
+        public string ContentTypeId { get; set; }
+        public string UrlSegment { get; set; }
+        public string ParentId { get; set; }
+        public string Name { get; set; }
+    }
+}
