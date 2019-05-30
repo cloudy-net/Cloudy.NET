19e167c4f5114ec7661761c61e694ec192e911b4
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-05-30 15:44:38 +0200

    Add Blob reader / writer

---
+++ Cloudy.CMS/BlobSupport/IBlobReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Cloudy.CMS.BlobSupport
+{
+    public interface IBlobReader
+    {
+        void Read(string id, Action<Stream> readAction);
+    }
+}
---
+++ Cloudy.CMS/BlobSupport/IBlobWriter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Cloudy.CMS.BlobSupport
+{
+    public interface IBlobWriter
+    {
+        void Write(string id, Action<Stream> writeAction);
+    }
+}
