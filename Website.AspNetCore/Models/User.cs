a3a32c83a3f370e5809f5ee3913f2145286bb5d5
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-05-31 20:51:19 +0200

    Add User class to example website

---
+++ Website.AspNetCore/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Cloudy.CMS.ContentSupport;
+using Cloudy.CMS.ContentTypeSupport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.AspNetCore.Models
+{
+    [Container("users")]
+    [ContentType("a4b8fd79-2432-4535-8ab8-5860c3bdb04d")]
+    public class User : IContent
+    {
+        public string Id { get; set; }
+        public string ContentTypeId { get; set; }
+
+        public string Username { get; set; }
+    }
+}
