b878b605bae872924298e13ba9fdb80c07536f10
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-06-06 12:59:18 +0200

    Add content mover

---
+++ Cloudy.CMS/ContentSupport/RepositorySupport/ContentMover.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cloudy.CMS.ContentSupport;
+using Cloudy.CMS.ContentSupport.RepositorySupport;
+
+namespace Cloudy.CMS.ContentSupport.RepositorySupport
+{
+    public class ContentMover : IContentMover
+    {
+        IContentUpdater ContentUpdater { get; }
+
+        public ContentMover(IContentUpdater contentUpdater)
+        {
+            ContentUpdater = contentUpdater;
+        }
+
+        public void Move(IContent content, string id)
+        {
+            var hierarchical = content as IHierarchical;
+
+            if(hierarchical == null)
+            {
+                throw new ArgumentException($"Content with Id {content.Id} ({content.ContentTypeId}) is not IHierarchical");
+            }
+
+            hierarchical.ParentId = id;
+
+            ContentUpdater.Update(content);
+        }
+    }
+}
---
+++ Cloudy.CMS/ContentSupport/RepositorySupport/IContentMover.cs
@@ -0,0 +1,9 @@
+﻿using Cloudy.CMS.ContentSupport;
+
+namespace Cloudy.CMS.ContentSupport.RepositorySupport
+{
+    public interface IContentMover
+    {
+        void Move(IContent content, string id);
+    }
+}
