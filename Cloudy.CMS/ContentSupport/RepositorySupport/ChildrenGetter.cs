a2c5b49b3c6aa116778d73e1ddc2386d9225a986
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-06-06 16:25:15 +0200

    Only retrieve chidren of matching content types

--- Cloudy.CMS/ContentSupport/RepositorySupport/ChildrenGetter.cs
+++ Cloudy.CMS/ContentSupport/RepositorySupport/ChildrenGetter.cs
@@ -24,27 +24,25 @@ namespace Cloudy.CMS.ContentSupport.RepositorySupport
 
         public IEnumerable<T> GetChildren<T>(string id, string language) where T : class
         {
+            var contentTypes = ContentTypeProvider.GetAll().Where(t => typeof(T).IsAssignableFrom(t.Type));
+
             var documents = ContainerProvider
                 .Get(ContainerConstants.Content)
                 .Find(
-                    Builders<Document>.Filter.Eq(new StringFieldDefinition<Document, string>("GlobalFacet.Interfaces.IHierarchical.Properties.ParentId"), id)
+                    Builders<Document>.Filter.And(
+                        Builders<Document>.Filter.Eq(new StringFieldDefinition<Document, string>("GlobalFacet.Interfaces.IHierarchical.Properties.ParentId"), id),
+                        Builders<Document>.Filter.In(new StringFieldDefinition<Document, string>("GlobalFacet.Interfaces.IContent.Properties.ContentTypeId"), contentTypes.Select(t => t.Id))
+                    )
                 )
                 .ToList();
 
-            var contentTypes = new Dictionary<string, ContentTypeDescriptor>();
             var result = new List<T>();
 
             foreach (var document in documents)
             {
                 var contentTypeId = document.GlobalFacet.Interfaces["IContent"].Properties["ContentTypeId"] as string;
-                ContentTypeDescriptor contentType;
 
-                if (!contentTypes.TryGetValue(contentTypeId, out contentType))
-                {
-                    contentType = ContentTypeProvider.Get(contentTypeId);
-                }
-
-                result.Add((T)ContentDeserializer.Deserialize(document, contentType, language));
+                result.Add((T)ContentDeserializer.Deserialize(document, ContentTypeProvider.Get(contentTypeId), language));
             }
 
             return result.AsReadOnly();
