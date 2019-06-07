77d5d1bb3a4258a0c0a8ea1b8c99c3d0b0a163aa
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-06-07 10:38:59 +0200

    Add BlobDeleter

---
+++ Cloudy.CMS/BlobSupport/IBlobDeleter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudy.CMS.BlobSupport
+{
+    public interface IBlobDeleter
+    {
+        void Delete(string id);
+    }
+}
