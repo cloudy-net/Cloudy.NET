cee5d9463ba550bac210405cdb82a8f74ea2ea8c
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-06-25 17:46:47 +0200

    Remove unnecessary nesting

--- Cloudy.CMS.UI/ChangesAppSupport/Scripts/changes-app.js
+++ Cloudy.CMS.UI/ChangesAppSupport/Scripts/changes-app.js
@@ -1,11 +1,11 @@
-﻿import App from '../../../Cloudy.CMS.Admin/Scripts/app.js';
-import Blade from '../../../Cloudy.CMS.Admin/Scripts/blade.js';
-import FormBuilder from '../../../Cloudy.CMS.Admin/FormSupport/Scripts/form-builder.js';
-import Button from '../../../Cloudy.CMS.Admin/Scripts/button.js';
-import DataTable from '../../../Cloudy.CMS.Admin/DataTableSupport/Scripts/data-table.js';
-import CopyAsTabSeparated from '../../../Cloudy.CMS.Admin/DataTableSupport/Scripts/copy-as-tab-separated.js';
-import ContextMenu from '../../../Cloudy.CMS.Admin/ContextMenuSupport/Scripts/context-menu.js';
-import notificationManager from '../../../Cloudy.CMS.Admin/NotificationSupport/Scripts/notification-manager.js';
+﻿import App from '../../Scripts/app.js';
+import Blade from '../../Scripts/blade.js';
+import FormBuilder from '../../FormSupport/Scripts/form-builder.js';
+import Button from '../../Scripts/button.js';
+import DataTable from '../../DataTableSupport/Scripts/data-table.js';
+import CopyAsTabSeparated from '../../DataTableSupport/Scripts/copy-as-tab-separated.js';
+import ContextMenu from '../../ContextMenuSupport/Scripts/context-menu.js';
+import notificationManager from '../../NotificationSupport/Scripts/notification-manager.js';
 
 
 
