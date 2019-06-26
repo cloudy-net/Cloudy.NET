8b19201eae07d9cd44b2040cd1bed5b6839c09f5
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-06-26 20:43:02 +0200

    Add Copy/Paste as JSON

--- Cloudy.CMS.UI/ContentAppSupport/Scripts/content-app.js
+++ Cloudy.CMS.UI/ContentAppSupport/Scripts/content-app.js
@@ -41,19 +41,20 @@ class ListContentTypesBlade extends Blade {
 
                     if (contentType.isSingleton) {
                         var formBuilder = new FormBuilder(`Cloudy.CMS.Content[type=${contentType.id}]`, app);
-                        var item = fetch(`ContentApp/GetSingleton?id=${contentType.id}`, {
+                        var content = fetch(`ContentApp/GetSingleton?id=${contentType.id}`, {
                             credentials: 'include',
                             method: 'Get',
                         })
                             .then(response => response.json());
 
-                        Promise.all([formBuilder.fieldModels, item]).then(results =>
+                        Promise.all([formBuilder.fieldModels, content]).then(results =>
                             button.onClick(() =>
                                 app.openAfter(
                                     new EditContentBlade(
                                         app,
                                         contentType,
                                         formBuilder,
+                                        results[1],
                                     ).onClose(() => button.setActive(false)),
                                     this
                                 )
@@ -119,10 +120,9 @@ class ListContentBlade extends Blade {
 class EditContentBlade extends Blade {
     onSaveCallbacks = [];
 
-    constructor(app, contentType, formBuilder, item) {
+    constructor(app, contentType, formBuilder, content) {
         super();
 
-        this.setTitle();
         if (contentType.IsRoutable) {
             var view = document.createElement('a');
             view.classList.add('poetry-ui-button');
@@ -132,43 +132,61 @@ class EditContentBlade extends Blade {
             this.setToolbar(view);
         }
 
-        var saveButton = new Button('Save').setDisabled();
-        var cancelButton = new Button('Cancel').onClick(() => app.close(this)).setDisabled();
+        if (!content) {
+            content = {};
+        }
 
-        this.setFooter(saveButton, cancelButton);
-
-        var init = item => {
-            if (item.Id) {
-                if (contentType.isNameable && item.name) {
-                    this.setTitle(`Edit ${item.name}`);
-                } else {
-                    this.setTitle(`Edit ${contentType.name}`);
-                }
-
-                if (contentType.isRoutable) {
-                    fetch(`ContentApp/GetUrl?id=${encodeURIComponent(item.id)}&contentTypeId=${encodeURIComponent(item.contentTypeId)}`, {
-                        credentials: 'include',
-                        method: 'GET',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        }
-                    })
-                        .then(response => response.text())
-                        .then(url => {
-                            if (!url) {
-                                return;
-                            }
-
-                            url = url.substr(1, url.length - 2);
-                            view.href = `${location.origin}${url}`;
-                            view.removeAttribute('disabled');
-                        });
-                }
+        if (content.Id) {
+            if (contentType.isNameable && content.name) {
+                this.setTitle(`Edit ${content.name}`);
             } else {
-                this.setTitle(`New ${contentType.name}`);
+                this.setTitle(`Edit ${contentType.name}`);
             }
 
-            var save = () =>
+            if (contentType.isRoutable) {
+                fetch(`ContentApp/GetUrl?id=${encodeURIComponent(content.id)}&contentTypeId=${encodeURIComponent(content.contentTypeId)}`, {
+                    credentials: 'include',
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                })
+                    .then(response => response.text())
+                    .then(url => {
+                        if (!url) {
+                            return;
+                        }
+
+                        url = url.substr(1, url.length - 2);
+                        view.href = `${location.origin}${url}`;
+                        view.removeAttribute('disabled');
+                    });
+            }
+        } else {
+            this.setTitle(`New ${contentType.name}`);
+        }
+
+        formBuilder.fieldModels.then(fieldModels =>
+            this.setContent(
+                new DataTable()
+                    .setBackend([...new Set(fieldModels.map(fieldModel => fieldModel.descriptor.group))].sort())
+                    .addColumn(c =>
+                        c.setHeader(() => 'Properties').setButton(group => {
+                            var button = new DataTableButton(group || 'General');
+
+                            button.onClick(() => {
+                                button.setActive();
+                                app.openAfter(new EditPropertyGroupBlade(app, formBuilder.build(content, { group: group }), group || 'Content').onClose(() => button.setActive(false)), this)
+                            });
+
+                            return button;
+                        })
+                    )
+            )
+        );
+
+        var saveButton = new Button('Save')
+            .onClick(() =>
                 fetch('ContentApp/Save', {
                     credentials: 'include',
                     method: 'POST',
@@ -176,41 +194,19 @@ class EditContentBlade extends Blade {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
-                        id: item.id,
+                        id: content.id,
                         contentTypeId: contentType.id,
-                        item: item
+                        item: content
                     })
                 })
-                .then(() => this.onSaveCallbacks.forEach(callback => callback(item)));
-
-            saveButton.onClick(() => save()).setDisabled(false);
-            cancelButton.setDisabled(false);
-
-            formBuilder.fieldModels.then(fieldModels =>
-                this.setContent(
-                    new DataTable()
-                        .setBackend([...new Set(fieldModels.map(fieldModel => fieldModel.descriptor.group))].sort())
-                        .addColumn(c =>
-                            c.setHeader(() => 'Properties').setButton(group => {
-                                var button = new DataTableButton(group || 'General');
-
-                                button.onClick(() => {
-                                    button.setActive();
-                                    app.openAfter(new EditPropertyGroupBlade(app, formBuilder.build(item, { group: group }), group || 'Content').onClose(() => button.setActive(false)), this)
-                                });
-
-                                return button;
-                            })
-                        )
-                )
+                    .then(() => this.onSaveCallbacks.forEach(callback => callback(content)))
             );
-        };
+        var cancelButton = new Button('Cancel').onClick(() => app.close(this));
+        var moreButton = new ContextMenu()
+            .addItem(item => item.setText('Copy').onClick(() => navigator.clipboard.writeText(JSON.stringify(content, null, '  '))))
+            .addItem(item => item.setText('Paste').onClick(() => { app.closeAfter(this); navigator.clipboard.readText().then(text => content = JSON.parse(text)); }));
 
-        if (item) {
-            init(item);
-        } else {
-            init({});
-        }
+        this.setFooter(saveButton, cancelButton, moreButton);
     }
 
     onSave(callback) {
