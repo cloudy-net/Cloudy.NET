275f18b560bd1bb3f45c0f13235a71552ba84d1c
Björn Göransson <bjorn.a.goransson@gmail.com>
2019-09-02 21:39:13 +0200

    Complete content route action finder logic

--- Cloudy.CMS/Routing/ContentRouteActionFinder.cs
+++ Cloudy.CMS/Routing/ContentRouteActionFinder.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using Cloudy.CMS.ContentSupport;
+using Cloudy.CMS.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 
@@ -18,7 +21,54 @@ namespace Cloudy.CMS.Routing
 
         public ControllerActionDescriptor Find(string controller, IContent content)
         {
-            throw new NotImplementedException();
+            var matchingActions = new List<ControllerActionDescriptor>();
+            var actionParameterTypes = new Dictionary<ControllerActionDescriptor, Type>();
+            var type = content.GetType();
+
+            foreach(var action in ActionDescriptorCollectionProvider.ActionDescriptors.Items.OfType<ControllerActionDescriptor>())
+            {
+                if(action.ControllerName != controller)
+                {
+                    continue;
+                }
+
+                var parameter = action.MethodInfo.GetParameters().SingleOrDefault(p => p.GetCustomAttribute<FromContentRouteAttribute>() != null);
+
+                if(parameter.ParameterType == type)
+                {
+                    return action;
+                }
+
+                if (parameter.ParameterType.IsAssignableFrom(type))
+                {
+                    matchingActions.Add(action);
+                    actionParameterTypes[action] = parameter.ParameterType;
+                }
+            }
+
+            if (!matchingActions.Any())
+            {
+                return null;
+            }
+
+            if(matchingActions.Count == 1)
+            {
+                return matchingActions.Single();
+            }
+
+            while(type != null)
+            {
+                type = type.BaseType;
+
+                var action = matchingActions.FirstOrDefault(a => actionParameterTypes[a] == type);
+
+                if(action != null)
+                {
+                    return action;
+                }
+            }
+
+            return null;
         }
     }
 }
--- Tests/ContentRouteActionFinderTests.cs
+++ Tests/ContentRouteActionFinderTests.cs
@@ -22,7 +22,7 @@ namespace Tests
         [Fact]
         public void RequiresMatchingControllerName()
         {
-            var action = new ControllerActionDescriptor { ControllerName = "lorem", ActionName = "a", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithAnnotatedTypeAParameter)) };
+            var action = new ControllerActionDescriptor { ControllerName = "lorem", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithTypeAParameter)) };
 
             var actions = Mock.Of<IActionDescriptorCollectionProvider>();
             Mock.Get(actions).SetupGet(a => a.ActionDescriptors).Returns(new ActionDescriptorCollection(new List<ActionDescriptor> { action }.AsReadOnly(), 0));
@@ -35,7 +35,7 @@ namespace Tests
         [Fact]
         public void RequiresContentRouteAttribute()
         {
-            var action = new ControllerActionDescriptor { ControllerName = "lorem", ActionName = "a", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithNonAnnotatedTypeAParameter)) };
+            var action = new ControllerActionDescriptor { ControllerName = "lorem", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithNonAnnotatedTypeAParameter)) };
 
             var actions = Mock.Of<IActionDescriptorCollectionProvider>();
             Mock.Get(actions).SetupGet(a => a.ActionDescriptors).Returns(new ActionDescriptorCollection(new List<ActionDescriptor> { action }.AsReadOnly(), 0));
@@ -48,8 +48,8 @@ namespace Tests
         [Fact]
         public void FindsActionWithExactContentRouteType()
         {
-            var actionA = new ControllerActionDescriptor { ControllerName = "lorem", ActionName = "a", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithAnnotatedTypeAParameter)) };
-            var actionB = new ControllerActionDescriptor { ControllerName = "lorem", ActionName = "b", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithAnnotatedTypeBParameter)) };
+            var actionA = new ControllerActionDescriptor { ControllerName = "lorem", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithTypeAParameter)) };
+            var actionB = new ControllerActionDescriptor { ControllerName = "lorem", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithTypeBParameter)) };
 
             var actions = Mock.Of<IActionDescriptorCollectionProvider>();
             Mock.Get(actions).SetupGet(a => a.ActionDescriptors).Returns(new ActionDescriptorCollection(new List<ActionDescriptor> { actionA, actionB }.AsReadOnly(), 0));
@@ -62,7 +62,7 @@ namespace Tests
         [Fact]
         public void FindsActionWithAssignableContentRouteType()
         {
-            var action = new ControllerActionDescriptor { ControllerName = "lorem", ActionName = "a", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithIContentParameter)) };
+            var action = new ControllerActionDescriptor { ControllerName = "lorem", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithIContentParameter)) };
 
             var actions = Mock.Of<IActionDescriptorCollectionProvider>();
             Mock.Get(actions).SetupGet(a => a.ActionDescriptors).Returns(new ActionDescriptorCollection(new List<ActionDescriptor> { action }.AsReadOnly(), 0));
@@ -70,6 +70,20 @@ namespace Tests
             var result = new ContentRouteActionFinder(actions).Find("lorem", new MyContentA());
 
             Assert.Same(action, result);
+        }
+
+        [Fact]
+        public void FindsActionWithMostSpecificContentRouteType()
+        {
+            var actionA = new ControllerActionDescriptor { ControllerName = "lorem", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithIContentParameter)) };
+            var actionB = new ControllerActionDescriptor { ControllerName = "lorem", MethodInfo = typeof(MyController).GetMethod(nameof(MyController.ActionWithTypeAParameter)) };
+
+            var actions = Mock.Of<IActionDescriptorCollectionProvider>();
+            Mock.Get(actions).SetupGet(a => a.ActionDescriptors).Returns(new ActionDescriptorCollection(new List<ActionDescriptor> { actionA, actionB }.AsReadOnly(), 0));
+
+            var result = new ContentRouteActionFinder(actions).Find("lorem", new MyContentADescendant());
+
+            Assert.Same(actionB, result);
         }
 
         class MyContentA : IContent {
@@ -83,11 +97,14 @@ namespace Tests
             public string ContentTypeId { get; set; }
         }
 
+        class MyContentADescendant : MyContentA { }
+
         class MyController
         {
             public void ActionWithNonAnnotatedTypeAParameter(MyContentA parameter) { }
-            public void ActionWithAnnotatedTypeAParameter([FromContentRoute] MyContentA parameter) { }
-            public void ActionWithAnnotatedTypeBParameter([FromContentRoute] MyContentB parameter) { }
+            public void ActionWithTypeAParameter([FromContentRoute] MyContentA parameter) { }
+            public void ActionWithTypeADescendantParameter([FromContentRoute] MyContentADescendant parameter) { }
+            public void ActionWithTypeBParameter([FromContentRoute] MyContentB parameter) { }
             public void ActionWithIContentParameter([FromContentRoute] IContent parameter) { }
         }
     }
