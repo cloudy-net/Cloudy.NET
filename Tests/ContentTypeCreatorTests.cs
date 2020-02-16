c8e725b1782995d015d1669e50f1ad8bdf63f0ce
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2020-02-16 23:04:08 +0100

    Create tests for abstract class with concrete subclass

--- Tests/ContentTypeCreatorTests.cs
+++ Tests/ContentTypeCreatorTests.cs
@@ -6,6 +6,7 @@ using Poetry;
 using Poetry.ComponentSupport;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -14,7 +15,7 @@ namespace Tests
     public class ContentTypeCreatorTests
     {
         [Fact]
-        public void IgnoresAbstractClasses()
+        public void ExcludesAbstractClass()
         {
             var component = new ComponentDescriptor("ipsum", new AssemblyWrapper(new List<Type> { typeof(Class_A_Abstract) }));
             var componentProvider = Mock.Of<IComponentProvider>();
@@ -25,11 +26,30 @@ namespace Tests
             Assert.Empty(result);
         }
 
+        [Fact]
+        public void IncludesAbstractClassIfSingleSubclassExists()
+        {
+            var component = new ComponentDescriptor("ipsum", new AssemblyWrapper(new List<Type> { typeof(Class_B_Extends_A) }));
+            var componentProvider = Mock.Of<IComponentProvider>();
+            Mock.Get(componentProvider).Setup(p => p.GetAll()).Returns(new List<ComponentDescriptor> { component });
+
+            var result = new ContentTypeCreator(componentProvider).Create();
+
+            Assert.Single(result);
+
+            var contentType = result.Single();
+
+            Assert.Equal("lorem", contentType.Id);
+            Assert.Same(typeof(Class_B_Extends_A), contentType.Type);
+        }
+
         [ContentType("lorem")]
         public abstract class Class_A_Abstract : IContent
         {
             public string Id { get; set; }
             public string ContentTypeId { get; set; }
         }
+
+        public class Class_B_Extends_A : Class_A_Abstract { }
     }
 }
