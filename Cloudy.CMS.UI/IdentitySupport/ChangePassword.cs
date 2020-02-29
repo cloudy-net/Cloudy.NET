28e84ec631b71fc7cb450403cbb6d837b7b3827b
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2020-02-29 08:29:04 +0100

    Add validation for changepassword

--- Cloudy.CMS.UI/IdentitySupport/ChangePassword.cs
+++ Cloudy.CMS.UI/IdentitySupport/ChangePassword.cs
@@ -10,9 +10,14 @@ namespace Cloudy.CMS.UI.IdentitySupport
     public class ChangePassword
     {
         [UIHint("hidden")]
+        [Required]
         public string UserId { get; set; }
         [UIHint("password")]
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
+        [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
+        [Required]
         [UIHint("password")]
         public string VerifyPassword { get; set; }
     }
--- Cloudy.CMS.UI/IdentitySupport/IdentityController.cs
+++ Cloudy.CMS.UI/IdentitySupport/IdentityController.cs
@@ -4,6 +4,7 @@ using Poetry.UI.FormSupport;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace Cloudy.CMS.UI.IdentitySupport
 {
     [Area("Cloudy.CMS")]
     [Route("Identity")]
-    public class IdentityController
+    public class IdentityController : Controller
     {
         UserManager UserManager { get; }
 
@@ -24,6 +25,15 @@ namespace Cloudy.CMS.UI.IdentitySupport
         [Route("ChangePassword")]
         public async Task<object> ChangePassword([FromBody] ChangePassword input)
         {
+            if (!ModelState.IsValid)
+            {
+                return new
+                {
+                    success = false,
+                    errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => new { description = e.ErrorMessage }),
+                };
+            }
+
             var user = await UserManager.FindByIdAsync(input.UserId);
 
             if (await UserManager.HasPasswordAsync(user))
