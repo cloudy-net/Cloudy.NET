936453d0a5560ee8292f98c343455d8eb3e07ac6
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2020-03-02 21:44:07 +0100

    Avoid collisions

--- Cloudy.CMS.UI/wwwroot/blade.js
+++ Cloudy.CMS.UI/wwwroot/blade.js
@@ -17,24 +17,24 @@ class Blade {
             this.element.dispatchEvent(new CustomEvent('cloudy-ui-close-blade', { bubbles: true, detail: { blade: this, parameters: event.detail.parameters } }));
         });
 
-        this.title = document.createElement('cloudy-ui-blade-title');
-        this.element.append(this.title);
-        this.titleText = document.createElement('cloudy-ui-blade-title-text');
-        this.title.append(this.titleText);
-        this.toolbar = document.createElement('cloudy-ui-blade-toolbar');
-        this.title.append(this.toolbar);
-        this.title.append(new BladeCloseButton().element);
+        this._title = document.createElement('cloudy-ui-blade-title');
+        this.element.append(this._title);
+        this._titleText = document.createElement('cloudy-ui-blade-title-text');
+        this._title.append(this._titleText);
+        this._toolbar = document.createElement('cloudy-ui-blade-toolbar');
+        this._title.append(this._toolbar);
+        this._title.append(new BladeCloseButton().element);
 
-        this.header = document.createElement('cloudy-ui-blade-header');
-        this.header.style.display = 'none';
-        this.element.append(this.header);
+        this._header = document.createElement('cloudy-ui-blade-header');
+        this._header.style.display = 'none';
+        this.element.append(this._header);
 
-        this.content = document.createElement('cloudy-ui-blade-content');
-        this.element.append(this.content);
+        this._content = document.createElement('cloudy-ui-blade-content');
+        this.element.append(this._content);
 
-        this.footer = document.createElement('cloudy-ui-blade-footer');
-        this.footer.style.display = 'none';
-        this.element.append(this.footer);
+        this._footer = document.createElement('cloudy-ui-blade-footer');
+        this._footer.style.display = 'none';
+        this.element.append(this._footer);
     }
 
     setFullWidth() {
@@ -43,30 +43,30 @@ class Blade {
     }
 
     setTitle(text) {
-        this.titleText.innerText = text;
+        this._titleText.innerText = text;
     }
 
     setToolbar(...items) {
-        this.toolbar.style.display = '';
-        [...this.toolbar.children].forEach(c => this.toolbar.removeChild(c));
-        items.forEach(item => this.toolbar.append(item.element || item));
+        this._toolbar.style.display = '';
+        [...this._toolbar.children].forEach(c => this._toolbar.removeChild(c));
+        items.forEach(item => this._toolbar.append(item.element || item));
     }
 
     setHeader(...items) {
-        this.header.style.display = '';
-        [...this.header.children].forEach(c => this.header.removeChild(c));
-        items.forEach(item => this.header.append(item.element || item));
+        this._header.style.display = '';
+        [...this._header.children].forEach(c => this._header.removeChild(c));
+        items.forEach(item => this._header.append(item.element || item));
     }
 
     setContent(...items) {
-        [...this.content.children].forEach(c => this.content.removeChild(c));
-        items.forEach(item => this.content.append(item.element || item));
+        [...this._content.children].forEach(c => this._content.removeChild(c));
+        items.forEach(item => this._content.append(item.element || item));
     }
 
     setFooter(...items) {
-        this.footer.style.display = '';
-        [...this.footer.children].forEach(c => this.footer.removeChild(c));
-        items.forEach(item => this.footer.append(item.element || item));
+        this._footer.style.display = '';
+        [...this._footer.children].forEach(c => this._footer.removeChild(c));
+        items.forEach(item => this._footer.append(item.element || item));
     }
 
     open() {
