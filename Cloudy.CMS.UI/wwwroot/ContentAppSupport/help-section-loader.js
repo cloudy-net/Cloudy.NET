203f9f19454295e02b3c2412de2a792108be65fc
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2020-03-02 22:04:03 +0100

    Add support for code block

--- Cloudy.CMS.UI/wwwroot/ContentAppSupport/help-section-loader.js
+++ Cloudy.CMS.UI/wwwroot/ContentAppSupport/help-section-loader.js
@@ -31,20 +31,31 @@ class HelpSectionLoader {
                 })
                 .catch(error => notificationManager.addNotification(item => item.setText(`Could not get help section ${id} (${error.name}: ${error.message})`)))
                 .then(content => {
-                    var image = `<img class="cloudy-ui-help-illustration" src="${content.image.src}" alt="${content.image.alt}">`;
-                    var header1 = `<h2 class="cloudy-ui-help-heading">${translate(content.heading)}</h2>`;
-                    var text1 = `<p>${translate(content.text)}</p>`;
-
-                    var buttons = document.createElement('div');
-                    buttons.style.textAlign = 'center';
-
-                    var actionsContent = Object.fromEntries(content.actions.map(a => [a.id, a.text]));
-
-                    Object.entries(actions).forEach(([key, callback]) => new Button(translate(actionsContent[key])).setPrimary().onClick(callback).appendTo(buttons));
-
                     var element = document.createElement('cloudy-ui-help-container');
-                    element.innerHTML = image + header1 + text1;
-                    element.append(buttons);
+
+                    element.insertAdjacentHTML('beforeend', `<img class="cloudy-ui-help-illustration" src="${content.image.src}" alt="${content.image.alt}">`);
+
+                    element.insertAdjacentHTML('beforeend', `<h2 class="cloudy-ui-help-heading">${translate(content.heading)}</h2>`);
+                    element.insertAdjacentHTML('beforeend', translate(content.text));
+
+                    if (content.actions.length) {
+                        var buttons = document.createElement('div');
+                        buttons.style.textAlign = 'center';
+                        element.append(buttons);
+
+                        var actionsContent = Object.fromEntries(content.actions.map(a => [a.id, a.text]));
+                        Object.entries(actions).forEach(([key, callback]) => new Button(translate(actionsContent[key])).setPrimary().onClick(callback).appendTo(buttons));
+                    }
+
+                    if (content.code) {
+                        var code = '<pre class="cloudy-ui-help-code">public class MyClass : …, INameable\n' +
+                            '{\n' +
+                            '    …\n' +
+                            '    public string Name { get; set; }\n' +
+                            '}</pre>';
+
+                        element.insertAdjacentHTML('beforeend', code);
+                    }
 
                     return element;
                 }));
