45c33db38bf1334ba7dec563fdd34260a67f1464
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2020-06-10 12:13:44 +0200

    Add IImageable

---
+++ Cloudy.CMS/ContentSupport/IImageable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudy.CMS.ContentSupport
+{
+    public interface IImageable
+    {
+        string Image { get; }
+    }
+}
