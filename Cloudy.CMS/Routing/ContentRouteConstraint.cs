7f5a1503ed408f7bf7d71ff6728bd601525414d4
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2020-06-18 09:44:34 +0200

    Early initialization/expanding of content types

--- Cloudy.CMS/Routing/ContentRouteConstraint.cs
+++ Cloudy.CMS/Routing/ContentRouteConstraint.cs
@@ -12,25 +12,24 @@ namespace Cloudy.CMS.Routing
     {
         IContentRouter ContentRouter { get; }
         IContentTypeExpander ContentTypeExpander { get; }
-        string ContentTypeorGroupId { get; }
+        IEnumerable<ContentTypeDescriptor> Types { get; }
 
         public ContentRouteConstraint(IContentRouter contentRouter, IContentTypeExpander contentTypeExpander, string contentTypeorGroupId = null)
         {
             ContentRouter = contentRouter;
             ContentTypeExpander = contentTypeExpander;
-            ContentTypeorGroupId = contentTypeorGroupId;
+
+            Types = contentTypeorGroupId != null ? ContentTypeExpander.Expand(contentTypeorGroupId) : Enumerable.Empty<ContentTypeDescriptor>();
+
+            if(Types == null)
+            {
+                throw new Exception($"Routing could not use the constraint :contentroute({contentTypeorGroupId}) because it did not resolve to a content type or group");
+            }
         }
 
         public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
         {
-            var types = ContentTypeorGroupId != null ? ContentTypeExpander.Expand(ContentTypeorGroupId) : Enumerable.Empty<ContentTypeDescriptor>();
-
-            if(types == null)
-            {
-                throw new Exception($"Routing could not use the constraint :contentroute({ContentTypeorGroupId}) because it did not resolve to a content type or group");
-            }
-
-            var content = ContentRouter.RouteContent(values[routeKey]?.ToString().Split('/') ?? Enumerable.Empty<string>(), types, null);
+            var content = ContentRouter.RouteContent(values[routeKey]?.ToString().Split('/') ?? Enumerable.Empty<string>(), Types, null);
 
             values["contentFromContentRoute"] = content;
 
