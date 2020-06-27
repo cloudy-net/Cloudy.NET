9297535ef6a61c302e101f273cdb58728c2bd1b9
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2020-06-27 10:54:26 +0200

    Delete hidden control

--- Cloudy.CMS.UI/wwwroot/FormSupport/Controls/hidden.js
+++
@@ -1,9 +0,0 @@
-﻿import FieldControl from '../field-control.js';
-
-class HiddenControl extends FieldControl {
-    constructor(fieldModel, value, app, blade) {
-        super(document.createElement('div'));
-    }
-}
-
-export default HiddenControl;
