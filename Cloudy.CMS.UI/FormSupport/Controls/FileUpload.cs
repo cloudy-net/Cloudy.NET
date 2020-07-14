47e8c7a2ce938bc896bbc1c166feeb3927bd0964
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2020-07-14 15:23:12 +0200

    Add FileUpload type

---
+++ Cloudy.CMS.UI/FormSupport/Controls/FileUpload.cs
@@ -0,0 +1,17 @@
+﻿using Cloudy.CMS.UI.FormSupport.ControlSupport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudy.CMS.UI.FormSupport.Controls
+{
+    [Control("fileupload", "FormSupport/Controls/fileupload.js")]
+    [MapControlToType(typeof(FileUpload))]
+    [MapControlToUIHint("fileupload(accept)")]
+    public class FileUpload
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
