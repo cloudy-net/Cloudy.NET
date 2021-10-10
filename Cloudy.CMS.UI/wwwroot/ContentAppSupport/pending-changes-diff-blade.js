1938a6919df8ffb6ed51c8c5f1bd112f7442ed60
Björn Göransson <bjorn.a.goransson@gmail.com>
2021-10-10 11:16:45 +0200

    Add fast-diff

--- Cloudy.CMS.UI/wwwroot/ContentAppSupport/pending-changes-diff-blade.js
+++ Cloudy.CMS.UI/wwwroot/ContentAppSupport/pending-changes-diff-blade.js
@@ -1,5 +1,6 @@
 ﻿import Blade from "../blade.js";
 import Button from "../button.js";
+import Diff from './lib/diff.js'
 
 
 
@@ -24,7 +25,37 @@ class PendingChangesDiffBlade extends Blade {
     }
 
     async open() {
-        this.setContent();
+        var form = document.createElement('div');
+        form.classList.add('cloudy-ui-form');
+
+        for (const changedField of this.change.changedFields) {
+            var element = document.createElement('div');
+            element.classList.add('cloudy-ui-form-field');
+
+            var heading = document.createElement('div');
+            heading.classList.add('cloudy-ui-form-field-label');
+            heading.innerText = changedField.name;
+            element.appendChild(heading);
+
+            var input = document.createElement('div');
+            input.classList.add('cloudy-ui-form-input');
+
+            var value = '';
+            for (const [state, segment] of Diff(changedField.originalValue, changedField.value, 0)) {
+                switch (state) {
+                    case Diff.DELETE: value += `<span class="cloudy-ui-diff-delete" style="background-color: #ffebe9; padding: 0 1px;">${segment}</span>`; break;
+                    case Diff.EQUAL: value += `<span>${segment}</span>`; break;
+                    case Diff.INSERT: value += `<span class="cloudy-ui-diff-insert" style="background-color: #e6ffec; padding: 0 1px;">${segment}</span>`; break;
+                }
+            }
+            input.innerHTML = value;
+                
+            element.append(input);
+
+            form.append(element);
+        }
+
+        this.setContent(form);
     }
 }
 
