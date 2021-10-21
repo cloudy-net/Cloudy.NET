e09879c69666bb5200cd8d2326775d6fc0bb4c7e
Björn Göransson <bjorn.a.goransson@gmail.com>
2021-10-21 20:02:17 +0200

    Add value converter for enumerables

---
+++ Cloudy.CMS/ContentSupport/Serialization/JsonValueConverter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloudy.CMS.ContentSupport.Serialization
+{
+    public class JsonValueConverter<T> : ValueConverter<T, string> where T: class
+    {
+        public JsonValueConverter() : base(
+            v => JsonConvert.SerializeObject(v),
+            v => (T)JsonConvert.DeserializeObject(v, typeof(T))
+        ) { }
+    }
+}
