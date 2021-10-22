1f8ea4be2307062144a3160f4b45f0004976d4da
Björn Göransson <bjorn.a.goransson@gmail.com>
2021-10-22 20:43:20 +0200

    Save inserts

--- Cloudy.CMS/ContentSupport/ContentInserter.cs
+++ Cloudy.CMS/ContentSupport/ContentInserter.cs
@@ -10,16 +10,19 @@ namespace Cloudy.CMS.ContentSupport
     public class ContentInserter : IContentInserter
     {
         IDbSetProvider DbSetProvider { get; }
+        IContextProvider ContextProvider { get; }
 
-        public ContentInserter(IDbSetProvider dbSetProvider)
+        public ContentInserter(IDbSetProvider dbSetProvider, IContextProvider contextProvider)
         {
             DbSetProvider = dbSetProvider;
+            ContextProvider = contextProvider;
         }
 
         public async Task InsertAsync(object content)
         {
             var dbSet = DbSetProvider.Get(content.GetType());
             await dbSet.AddAsync(content).ConfigureAwait(false);
+            await ContextProvider.GetFor(content.GetType()).Context.SaveChangesAsync().ConfigureAwait(false);
         }
     }
 }
