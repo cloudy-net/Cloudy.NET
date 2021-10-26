a9a52220f9f8b2cf3c79467eefb25a7fc6b4a797
Björn Göransson <bjorn.a.goransson@gmail.com>
2021-10-26 20:11:47 +0200

    Adjust paths

--- Cloudy.CMS.UI/wwwroot/ContentAppSupport/utils/content-saver.js
+++ Cloudy.CMS.UI/wwwroot/ContentAppSupport/utils/content-saver.js
@@ -1,6 +1,6 @@
-import "./utils/content-type-provider.js";
-import notificationManager from "../NotificationSupport/notification-manager.js";
-import urlFetcher from "../url-fetcher.js";
+import "./content-type-provider.js";
+import notificationManager from "../../NotificationSupport/notification-manager.js";
+import urlFetcher from "../../url-fetcher.js";
 
 /* CONTENT SAVER */
 
