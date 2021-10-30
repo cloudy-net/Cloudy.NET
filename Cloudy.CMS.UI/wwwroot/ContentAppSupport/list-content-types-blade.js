d11f0c5f09b413692ff92a4bba32f2ab312e9d1e
Björn Göransson <bjorn.a.goransson@gmail.com>
2021-10-30 19:55:44 +0200

    Remove GUID generator function

--- Cloudy.CMS.UI/wwwroot/ContentAppSupport/list-content-types-blade.js
+++ Cloudy.CMS.UI/wwwroot/ContentAppSupport/list-content-types-blade.js
@@ -11,15 +11,6 @@ import ListItem from '../ListSupport/list-item.js';
 
 /* LIST CONTENT TYPES BLADE */
 
-var guid = uuidv4();
-
-function uuidv4() { // https://stackoverflow.com/a/2117523
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
 class ListContentTypesBlade extends Blade {
     action = null;
     actions = null;
