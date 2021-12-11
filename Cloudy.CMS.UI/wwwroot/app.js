a90e8c80def0d8fc4e2aac3b9d2ea195a9aafe1f
Björn Göransson <bjorn.a.goransson@gmail.com>
2021-12-11 17:10:15 +0100

    Add List Content component

--- Cloudy.CMS.UI/wwwroot/app.js
+++ Cloudy.CMS.UI/wwwroot/app.js
@@ -3,13 +3,14 @@ import html from './html.js';
 import ContentTypeProvider from './ContentAppSupport/utils/content-type-provider.js';
 import List from './ListSupport/list.js';
 import ListItem from './ListSupport/list-item.js';
+import ListContent from './list-content.js';
 
 function App(props) {
     const [editingContentType, editContentType] = useState(null);
     const [items, setItems] = useState([]);
 
     useEffect(() => {
-        ContentTypeProvider.getAll().then(data => setItems(data));
+        ContentTypeProvider.getAll().then(items => setItems(items));
     }, []);
 
     return html`
@@ -24,8 +25,9 @@ function App(props) {
                 <cloudy-ui-blade>
                     <cloudy-ui-blade-title><cloudy-ui-blade-title-text>What to edit<//><//>
                     <${List}>
-				        ${items.map(result => html`<${ListItem} active=${result == editingContentType} text=${result.name} onclick=${() => editContentType(result)} />`)}
+				        ${items.map(item => html`<${ListItem} active=${item == editingContentType} text=${item.name} onclick=${() => editContentType(item)} />`)}
                     <//>
+                    ${editingContentType && html`<${ListContent} contentType=${editingContentType}/>`}
                 <//>
             <//>
         <//>
