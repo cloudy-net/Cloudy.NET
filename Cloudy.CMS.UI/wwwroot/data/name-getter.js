f3fb3af7a6a039f24fb703444bd19017b148472b
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-04-27 23:55:45 +0200

    Implement get name from state

--- Cloudy.CMS.UI/wwwroot/data/name-getter.js
+++ Cloudy.CMS.UI/wwwroot/data/name-getter.js
@@ -27,6 +27,44 @@ class NameGetter {
 
         return name;
     }
+
+    getNameOfState(state, contentType) {
+        if (!state) {
+            throw new Error('No state was supplied');
+        }
+        if (!contentType) {
+            throw new Error('No content type was supplied');
+        }
+
+        let name = null;
+
+        const getProperty = key => {
+            const changedField = state.changedFields.find(f => f.path.length == 1 && f.path[0] == key && f.type == 'simple' && f.operation == 'set');
+
+            if (changedField) {
+                return changedField.value;
+            }
+
+            return state.referenceValues[key];
+        };
+
+        if (!contentType.isSingleton) {
+            if (contentType.isNameable) {
+                name = contentType.nameablePropertyName ? getProperty(contentType.nameablePropertyName) : getProperty('Name');
+            }
+        }
+        if (!name) {
+            if (contentType.isSingleton) {
+                name = contentType.name;
+            } else if (state.contentReference.newContentKey) {
+                name = `${contentType.name} ${state.contentReference.newContentKey}`;
+            } else {
+                name = `${contentType.name} ${JSON.stringify(state.contentReference.keys)}`;
+            }
+        }
+
+        return name;
+    }
 }
 
 export default new NameGetter();
--- Cloudy.CMS.UI/wwwroot/diff/pending-changes.js
+++ Cloudy.CMS.UI/wwwroot/diff/pending-changes.js
@@ -16,13 +16,15 @@ function PendingChanges({ renderIf }) {
         .map(c => ({ contentType: c, changes: states.filter(s => s.contentReference.contentTypeId == c.id) }))
         .filter(g => g.changes.length);
 
-    const itemText = (item, contentType) => item.remove ? 'Slated for removal' : item.contentId ? `Changed fields: ${item.changedFields.length}` : `New ${contentType.lowerCaseName} (created ${item.referenceDate.toLocaleString()})`;
+    const getSubText = (state, contentType) => state.remove ? 'Slated for removal' : state.contentId ? `Changed fields: ${state.changedFields.length}` : `New ${contentType.lowerCaseName} (created ${state.referenceDate.toLocaleString()})`;
+    const renderItems = (states, contentType) => states.map(state => html`<${ListItem} text=${nameGetter.getNameOfState(state, contentType)} subtext=${getSubText(state, contentType)} onclick=${() => setDiffData(state.change)} />`)
 
     return html`
         <${Blade} title='Pending changes' onClose=${() => setShowDiffBlade(false)}>
             <cloudy-ui-blade-content>
                 ${!groups.length ? html`<cloudy-ui-list-sub-header>No more pending changes<//>` : null}
-                ${groups.length ? groups.map(group => html`${groups.length > 1 ? html`<cloudy-ui-list-sub-header>${group.contentType.name}<//>` : null}${group.changes.map(item => html`<${ListItem} text=${nameGetter.getNameOf(item, group.contentType)} subtext=${itemText(item, group.contentType)} onclick=${() => setDiffData(item.change)} />`)}`) : null}
+                ${groups.length ? groups.map(group => html`${groups.length > 1 ?
+                    html`<cloudy-ui-list-sub-header>${group.contentType.name}<//>` : null}${renderItems(group.changes, group.contentType)}`) : null}
             <//>
             <cloudy-ui-blade-footer>
                 <cloudy-ui-button tabindex="0" class="primary" style="margin-left: auto;" disabled=${!groups.length} onclick=${() => saveChanges()}>Save</cloudy-ui-button>
