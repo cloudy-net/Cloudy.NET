e91a780f284969865612a46197a824642a385bff
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-04-30 10:30:08 +0200

    Cleanup

--- Cloudy.CMS.UI/wwwroot/edit-content/state-context-provider.js
+++ Cloudy.CMS.UI/wwwroot/edit-content/state-context-provider.js
@@ -1,6 +1,5 @@
 import html from '../util/html.js';
 import StateContext from './state-context.js';
-import { useEffect, useState } from '../lib/preact.hooks.module.js';
 import stateManager from './state-manager.js';
 
 function StateContextProvider({ renderIf, children, contentReference }) {
