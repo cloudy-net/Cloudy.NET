a9b2113f533163e9debb242f9f5b72ecefd8f3aa
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-05-02 04:57:02 +0200

    Add changed badge

--- Cloudy.CMS.UI/wwwroot/edit-content/simple-field.js
+++ Cloudy.CMS.UI/wwwroot/edit-content/simple-field.js
@@ -1,6 +1,7 @@
 
 import propertyGetter from '../data/property-getter.js';
 import html from '../util/html.js';
+import arrayEquals from "../util/array-equals.js";
 
 export default function SimpleField({ fieldModel, state, path }) {
     if (fieldModel.descriptor.embeddedFormId && !fieldModel.descriptor.isSortable) {
@@ -10,9 +11,15 @@ export default function SimpleField({ fieldModel, state, path }) {
 
     const emitEvent = (element, value) => element.dispatchEvent(new CustomEvent('cloudy-ui-form-change', { bubbles: true, detail: { change: { path, type: 'simple', operation: 'set', value } } }))
 
+    const getChangeBadge = () => {
+        if (state.changedFields.find(ch => arrayEquals(ch.path, path))) {
+            return html`<cloudy-ui-form-field-changed title="This field has pending changes."><//>`;
+        }
+    };
+
     return html`
         <div class="cloudy-ui-form-field cloudy-ui-simple">
-            <div class="cloudy-ui-form-field-label">${fieldModel.descriptor.label || fieldModel.descriptor.id}</div>
+            <div class="cloudy-ui-form-field-label">${fieldModel.descriptor.label || fieldModel.descriptor.id}${getChangeBadge()}</div>
             <${fieldModel.controlType} onchange=${emitEvent} fieldModel=${fieldModel} initialValue=${propertyGetter.get(state, path)}/>
         </div>
     `;
