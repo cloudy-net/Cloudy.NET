bce5fe65d22d4a70e2df8e2f94219734c8aed028
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-05-23 08:34:01 +0200

    Refactor form files

--- Cloudy.CMS.UI/wwwroot/edit-content/form/field-model-context-provider.js
+++ Cloudy.CMS.UI/wwwroot/edit-content/form/field-model-context-provider.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from '../lib/preact.hooks.module.js';
-import html from '../util/html.js';
+import { useEffect, useState } from '../../lib/preact.hooks.module.js';
+import html from '../../util/html.js';
 import FieldModelContext from './field-model-context.js';
-import stateManager from './state-manager.js';
 
 function FieldModelContextProvider({ children, contentReference }) {
     const [state, setState] = useState(null);
 
     useEffect(() => {
+        field
         setState(stateManager.getState(contentReference));
         
         
