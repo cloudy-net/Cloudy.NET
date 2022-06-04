df464de50f0d1a5fe2962eaac6fe88d8a0659507
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-06-04 11:51:53 +0200

    Remove unused event

--- Cloudy.CMS.UI/wwwroot/edit-content/form/simple-field.js
+++ Cloudy.CMS.UI/wwwroot/edit-content/form/simple-field.js
@@ -3,8 +3,6 @@ import html from '../../util/html.js';
 import arrayEquals from "../../util/array-equals.js";
 
 export default function SimpleField({ fieldModel, state, path }) {
-    const emitEvent = (element, value) => element.dispatchEvent(new CustomEvent('cloudy-ui-form-simple-change', { bubbles: true, detail: { change: { path, value } } }))
-
     const getChangeBadge = () => {
         return html`<cloudy-ui-change-badge class=${state.simpleChanges && state.simpleChanges.find(ch => arrayEquals(ch.path, path)) ? 'cloudy-ui-unchanged' : null} title="This field has pending changes."><//>`;
     };
@@ -12,7 +10,7 @@ export default function SimpleField({ fieldModel, state, path }) {
     return html`
         <div class="cloudy-ui-form-field cloudy-ui-simple">
             <div class="cloudy-ui-form-field-label">${fieldModel.descriptor.label || fieldModel.descriptor.id}${getChangeBadge()}</div>
-            <${fieldModel.controlType} onchange=${emitEvent} path=${path} fieldModel=${fieldModel} state=${state}/>
+            <${fieldModel.controlType} path=${path} fieldModel=${fieldModel} state=${state}/>
         </div>
     `;
 }
