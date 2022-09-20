e55c831e69d4955dbe347d82667772992af69836
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-09-20 19:45:22 +0200

    Link pages

---
+++ Cloudy.CMS.UI/Pages/Admin/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cloudy.CMS.UI.Pages.Admin
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
