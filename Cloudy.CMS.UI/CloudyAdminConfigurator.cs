9c80f564ceb9540d6c3cbd72f77116890718ca9c
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-01 21:19:09 +0200

    Cleanup

--- Cloudy.CMS.UI/CloudyAdminConfigurator.cs
+++ Cloudy.CMS.UI/CloudyAdminConfigurator.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.FileProviders;
-using Cloudy.CMS.UI.AspNetCore;
 using System;
 
 namespace Cloudy.CMS.UI
@@ -11,20 +10,6 @@ namespace Cloudy.CMS.UI
         public CloudyAdminConfigurator(CloudyAdminOptions options)
         {
             Options = options;
-        }
-
-        public CloudyAdminConfigurator WithBasePath(string adminBasePath)
-        {
-            Options.BasePath = adminBasePath.TrimEnd('/');
-
-            return this;
-        }
-
-        public CloudyAdminConfigurator WithHelpSectionsFrom(string baseUri)
-        {
-            Options.HelpSectionBaseUri = baseUri.TrimEnd('/');
-
-            return this;
         }
 
         public CloudyAdminConfigurator Unprotect()
--- Cloudy.CMS.UI/CloudyAdminOptions.cs
+++ Cloudy.CMS.UI/CloudyAdminOptions.cs
@@ -1,6 +1,5 @@
 ﻿
 using Microsoft.Extensions.FileProviders;
-using Cloudy.CMS.UI.AspNetCore;
 using System;
 
 namespace Cloudy.CMS.UI
@@ -8,7 +7,5 @@ namespace Cloudy.CMS.UI
     public class CloudyAdminOptions
     {
         public bool Unprotect { get; set; }
-        public string BasePath { get; set; } = "/Admin";
-        public string HelpSectionBaseUri { get; set; } = "https://cloudy-cms.net/help-sections";
     }
 }
