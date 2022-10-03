371a7275930bb0da0ff9c71cbffda3df654b3bbd
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-03 22:09:47 +0200

    Render view to string

--- Cloudy.CMS.UI/List/ListResultController.cs
+++ Cloudy.CMS.UI/List/ListResultController.cs
@@ -1,39 +1,95 @@
 ﻿using Cloudy.CMS.ContentSupport.RepositorySupport.Context;
 using Cloudy.CMS.ContentTypeSupport;
+using Cloudy.CMS.UI.FormSupport.FieldSupport;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.DynamicLinq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Linq.Dynamic.Core;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Cloudy.CMS.UI.List
 {
-    public class ListResultController
+    public class ListResultController : Controller
     {
+        IPropertyDefinitionProvider PropertyDefinitionProvider { get; }
+        IContentTypeProvider ContentTypeProvider { get; }
+        IContextCreator ContextCreator { get; }
+        ICompositeViewEngine CompositeViewEngine { get; }
+
+        public ListResultController(IPropertyDefinitionProvider propertyDefinitionProvider, IContentTypeProvider contentTypeProvider, IContextCreator contextCreator, ICompositeViewEngine compositeViewEngine)
+        {
+            PropertyDefinitionProvider = propertyDefinitionProvider;
+            ContentTypeProvider = contentTypeProvider;
+            ContextCreator = contextCreator;
+            CompositeViewEngine = compositeViewEngine;
+        }
+
         [HttpGet]
         [Area("Admin")]
         [Route("/{area}/api/list/result")]
-        public async Task<ListResultResponse> ListResult(string contentType, string query, [FromServices] IContentTypeProvider contentTypeProvider, [FromServices] IContextCreator contextCreator)
+        public async Task<ListResultResponse> ListResult(string contentType, string columns)
         {
-            var type = contentTypeProvider.Get(contentType);
+            var columnNames = columns.Split(",");
 
-            using var context = contextCreator.CreateFor(type.Type);
+            var type = ContentTypeProvider.Get(contentType);
+
+            using var context = ContextCreator.CreateFor(type.Type);
 
             var dbSet = (IQueryable)context.GetDbSet(type.Type).DbSet;
 
-            if(query != null)
-            {
-                dbSet = dbSet.Where($"Name.Contains(@0)", query);
-            }
+            //if(query != null)
+            //{
+            //    dbSet = dbSet.Where($"Name.Contains(@0)", query);
+            //}
 
             var totalCount = await dbSet.CountAsync().ConfigureAwait(false);
 
+            var result = new List<IDictionary<string, string>>();
+
+            var propertyDefinitions = PropertyDefinitionProvider.GetFor(type.Name).Where(p => columnNames.Contains(p.Name));
+
+            //await foreach(var instance in (IAsyncEnumerable<object>)dbSet)
+            foreach(var instance in new List<object> { new MyPage { Name = "Lorem" } })
+            {
+                var row = new Dictionary<string, string>();
+
+
+                foreach(var propertyDefinition in propertyDefinitions)
+                {
+                    var partialViewName = $"Columns/Text";
+                    var viewResult = CompositeViewEngine.FindView(ControllerContext, partialViewName, false);
+
+                    if (!viewResult.Success)
+                    {
+                        throw new InvalidOperationException($"The partial view '{partialViewName}' was not found. The following locations were searched:{Environment.NewLine}{string.Join(Environment.NewLine, viewResult.SearchedLocations)}");
+                    }
+
+                    var view = viewResult.View;
+
+                    using (var writer = new StringWriter())
+                    using (view as IDisposable)
+                    {
+                        var viewContext = new ViewContext(ControllerContext, view, ViewData, TempData, writer, new HtmlHelperOptions());
+                        await view.RenderAsync(viewContext).ConfigureAwait(false);
+                        row[propertyDefinition.Name] = writer.ToString();
+                    }
+                }
+
+                result.Add(row);
+            }
+
             return new ListResultResponse
             {
-                Items = await dbSet.Page(1, 50).ToDynamicListAsync().ConfigureAwait(false),
+                Items = result,
                 TotalCount = totalCount,
             };
         }
@@ -47,6 +103,11 @@ namespace Cloudy.CMS.UI.List
         {
             public IEnumerable<object> Items { get; set; }
             public int TotalCount { get; set; }
+        }
+
+        private class MyPage
+        {
+            public string Name { get; set; }
         }
     }
 }
