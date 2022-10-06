8463d9f210bf2a80b90b446cd15f27b5d194c47e
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-06 20:18:10 +0200

    Make vite dev run

--- Cloudy.CMS.UI/wwwroot-src/main.jsx
+++ Cloudy.CMS.UI/wwwroot-src/main.jsx
@@ -1,7 +1,7 @@
 import { render } from 'preact'
 import { ListTable } from './list-table'
 import './main.scss'
-
+console.log("hello")
 const element = document.querySelector('.list-table');
 const settings = JSON.parse(element.getAttribute('settings'));
 
