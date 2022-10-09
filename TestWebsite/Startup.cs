fc611a56966e9cb8e7bea1f7595c444d77a1d0ad
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-09 15:54:56 +0200

    Don't generate seed data dynamically

--- TestWebsite/Startup.cs
+++ TestWebsite/Startup.cs
@@ -47,13 +47,33 @@ namespace TestWebsite
             using (var scope = app.ApplicationServices.CreateScope())
             {
                 var context = scope.ServiceProvider.GetService<PageContext>();
-                var random = new Random();
-                var lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".Split(' ');
 
-                for (var i = 0; i < 30; i++)
-                {
-                    context.Add(new Page { Name = string.Join(" ", lipsum.OrderBy(t => random.Next()).Take(random.Next(5 + 2))).TrimEnd(',', '.') });
-                }
+                context.Pages.Add(new Page { Id = new Guid("e6fd53d8-c7de-4355-ae21-c588b2673c5c"), Name = "occaecat ullamco minim" });
+                context.Pages.Add(new Page { Id = new Guid("c31836f7-830e-44d3-b231-97d48cf44df3"), Name = "esse ea Excepteur in minim dolore" });
+                context.Pages.Add(new Page { Id = new Guid("9ab7ea51-f66a-426e-a18c-51316a396a9e"), Name = "dolor" });
+                context.Pages.Add(new Page { Id = new Guid("0c1c40a9-ee61-4071-a1e5-17a2079d882a"), Name = "ut et occaecat ad sit" });
+                context.Pages.Add(new Page { Id = new Guid("2738ca18-3061-4858-8bed-8ad418447fc7"), Name = "in" });
+                context.Pages.Add(new Page { Id = new Guid("66e44063-a69f-41ac-82bf-220d70709801"), Name = "eiusmod culpa aute Excepteur est" });
+                context.Pages.Add(new Page { Id = new Guid("cee2b628-71a8-43e9-88e4-ba51bcf3e940"), Name = "laboris anim ut" });
+                context.Pages.Add(new Page { Id = new Guid("0ff35319-ad55-44e0-83b7-f1f8d7b93a41"), Name = "officia veniam, Duis sint ullamco quis" });
+                context.Pages.Add(new Page { Id = new Guid("c9ad7601-d0a9-47e4-9515-a2048ba502ad"), Name = "minim aliqua. et esse" });
+                context.Pages.Add(new Page { Id = new Guid("5dfd0c64-d1ec-48a4-b50d-5fab3f2606f6"), Name = "Duis non deserunt cillum" });
+                context.Pages.Add(new Page { Id = new Guid("cf6053ac-bf4d-417c-90c7-ec8379e23d47"), Name = "anim dolor irure ut" });
+                context.Pages.Add(new Page { Id = new Guid("5835789f-91b0-4b74-a399-1099b21a0b27"), Name = "nulla dolore do cillum" });
+                context.Pages.Add(new Page { Id = new Guid("4fc870b6-b3d1-443e-acf5-8eab1ff1c4a4"), Name = "nostrud nisi est" });
+                context.Pages.Add(new Page { Id = new Guid("2b8c9ecf-1b3c-4059-96ba-972e80ad2a98"), Name = "nostrud ut voluptate sit eu" });
+                context.Pages.Add(new Page { Id = new Guid("4b3d6edc-6ce4-481a-b152-85e992490e6e"), Name = "Lorem Duis esse" });
+                context.Pages.Add(new Page { Id = new Guid("f3c48571-75ef-4b33-908e-5fa3d5c50901"), Name = "magna" });
+                context.Pages.Add(new Page { Id = new Guid("447c9f5f-5ad2-470e-a562-1facb2df5741"), Name = "mollit ea aliqua. elit, sed minim" });
+                context.Pages.Add(new Page { Id = new Guid("8f107afa-dc60-4916-8d57-0d262984d450"), Name = "tempor consectetur incididunt mollit" });
+                context.Pages.Add(new Page { Id = new Guid("253b28a9-2bf6-4015-b249-f24a38232357"), Name = "cillum eiusmod qui eu" });
+                context.Pages.Add(new Page { Id = new Guid("201eb37a-b3f0-4764-848b-e0a964b0ca4d"), Name = "velit" });
+                context.Pages.Add(new Page { Id = new Guid("b7e84c3a-97dc-47d5-ae69-abc00a7487b9"), Name = "consectetur nostrud Lorem" });
+                context.Pages.Add(new Page { Id = new Guid("6a7cd1b2-e262-48a4-9d80-7f722833a6a0"), Name = "aliquip tempor" });
+                context.Pages.Add(new Page { Id = new Guid("28b2affa-6aa9-475d-ad85-a8bc223d965b"), Name = "adipiscing aliquip dolore sit sunt Lorem" });
+                context.Pages.Add(new Page { Id = new Guid("2054a8df-b475-41c9-aeee-ad1a0c505eaa"), Name = "aliqua. in veniam, in dolore" });
+                context.Pages.Add(new Page { Id = new Guid("7f77f50d-885c-446b-9a61-49d29e3cf2d2"), Name = "ut reprehenderit" });
+                context.Pages.Add(new Page { Id = new Guid("b5bf348e-cb21-4714-a331-ddedaf19a0db"), Name = "ut consectetur" });
 
                 context.SaveChanges();
             }
