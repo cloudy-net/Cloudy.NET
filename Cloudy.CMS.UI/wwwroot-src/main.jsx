6aa2b32a3267ac94661560bb73a3c0a39aafacf4
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-19 19:50:23 +0200

    Add HTML control support

--- Cloudy.CMS.UI/wwwroot-src/main.jsx
+++ Cloudy.CMS.UI/wwwroot-src/main.jsx
@@ -7,8 +7,19 @@ import './main.scss'
 window.viteIsLoaded = true;
 
 document.querySelectorAll('.list-page-table').forEach(element =>
-    render(<Table {...JSON.parse(element.getAttribute('settings') || '{}')} />, element)
+  render(<Table {...JSON.parse(element.getAttribute('settings') || '{}')} />, element)
 );
 document.querySelectorAll('.select-one-control').forEach(element =>
-    render(<SelectOne {...JSON.parse(element.getAttribute('settings') || '{}')} />, element)
+  render(<SelectOne {...JSON.parse(element.getAttribute('settings') || '{}')} />, element)
 );
+document.querySelectorAll('.html-control').forEach(element => {
+  var input = document.querySelector(element.getAttribute('for'));
+  var quill = new Quill(element, {
+    modules: {
+      // toolbar: '#toolbar'
+    },
+    theme: 'snow'
+  });
+  quill.clipboard.dangerouslyPasteHTML(input.value)
+  quill.on('text-change', () => input.value = quill.root.innerHTML);
+});
