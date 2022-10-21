54cdc1bf12e2ab7eba63e38c4f4c83ffe141a396
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-21 21:43:11 +0200

    Add dropdown appearance to enum dropdown

--- Cloudy.CMS.UI/wwwroot-src/enum-dropdown/enum-dropdown.jsx
+++ Cloudy.CMS.UI/wwwroot-src/enum-dropdown/enum-dropdown.jsx
@@ -1,6 +1,6 @@
 export default ({ controlId, controlName, nullable, value, options }) => {
   return <>
-    <select class="form-control" id={controlId} name={controlName} value={value}>
+    <select class="form-select" id={controlId} name={controlName} value={value}>
       {nullable && <option value="">(empty)</option>}
       {(options ? Object.entries(options) : []).map(([name, label]) => <option value={name}>{label}</option>)}
     </select>
