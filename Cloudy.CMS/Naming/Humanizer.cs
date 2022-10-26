89f9f105a06006b34f85e349ae4f3a961a12c043
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-10-26 18:41:00 +0200

    Humanizer should handle kebab case

--- Cloudy.CMS/Naming/Humanizer.cs
+++ Cloudy.CMS/Naming/Humanizer.cs
@@ -10,11 +10,13 @@ namespace Cloudy.CMS.Naming
     {
         public string Humanize(string value)
         {
-            value = Regex.Replace(value, @"([A-Z])([A-Z][a-z])", "$1 $2");
-            value = Regex.Replace(value, @"([a-z])([A-Z])", "$1 $2");
-            value = Regex.Replace(value, @"([a-z])([0-9])", "$1 $2");
-            value = Regex.Replace(value, @"([0-9])([A-Z])", "$1 $2");
-            value = Regex.Replace(value, @" [A-Z][a-z]", m => m.Value.ToLower());
+            value = value[..1].ToUpper() + value[1..]; // uppercase first letter
+            value = value.Replace("-", " "); // split words on hyphen/dash (kebab-case)
+            value = Regex.Replace(value, @"([A-Z])([A-Z][a-z])", "$1 $2"); // separate uppercase letters and succeeding capitalized word (eg. UIHint)
+            value = Regex.Replace(value, @"([a-z])([A-Z])", "$1 $2"); // separate lowercase letter and uppercase letter (camel case)
+            value = Regex.Replace(value, @"([a-z])([0-9])", "$1 $2"); // separate lowercase letter and succeeding digit (lorem0)
+            value = Regex.Replace(value, @"([0-9])([A-Z])", "$1 $2"); // separate digit and succeeding uppercase letter (0Text)
+            value = Regex.Replace(value, @" [A-Z][a-z]", m => m.Value.ToLower()); // lowercase all capital initial letters after first word
 
             return value;
         }
