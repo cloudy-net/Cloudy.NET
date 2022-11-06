2eafd45faff8294ad3b45173e64dae927afd752d
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-11-06 20:21:29 +0100

    Hide upload dropdown

--- Cloudy.CMS.UI/wwwroot-src/media-picker/media-picker-menu.jsx
+++ Cloudy.CMS.UI/wwwroot-src/media-picker/media-picker-menu.jsx
@@ -141,14 +141,14 @@ export default ({ provider, value, onSelect }) => {
         <li class="ms-auto">
           <div class="btn-group">
             <button type="button" class="btn btn-sm btn-primary" onClick={() => selectFile()}>Upload</button>
-            <button type="button" class="btn btn-sm btn-primary dropdown-toggle dropdown-toggle-split"></button>
+            {/* <button type="button" class="btn btn-sm btn-primary dropdown-toggle dropdown-toggle-split"></button>
             <div class="dropdown-menu">
               <a class="dropdown-item" href="#">Action</a>
               <a class="dropdown-item" href="#">Another action</a>
               <a class="dropdown-item" href="#">Something else here</a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="#">Separated link</a>
-            </div>
+            </div> */}
           </div>
         </li>
       </ul>
