82236c74acffe2a555c31c0341029c647a205f2f
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-11-09 22:41:45 +0100

    Delete ContentMover

--- Cloudy.CMS/ContentSupport/RepositorySupport/Methods/IContentMover.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Cloudy.CMS.ContentSupport.RepositorySupport.Methods
-{
-    public interface IContentMover
-    {
-        Task MoveAsync(object content, params object[] keyValues);
-    }
-}
