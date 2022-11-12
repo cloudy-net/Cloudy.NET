132fba8e049de2fce365aea8c3129b1de7aaacca
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-11-12 11:14:40 +0100

    Don't inject singleton instances directly

--- Cloudy.CMS/SingletonSupport/DependencyInjector.cs
+++ Cloudy.CMS/SingletonSupport/DependencyInjector.cs
@@ -15,18 +15,7 @@ namespace Cloudy.CMS.SingletonSupport
     {
         public void InjectDependencies(IServiceCollection services)
         {
-            foreach (var context in ContextDescriptorProvider.GetAll())
-            {
-                foreach (var dbSet in context.DbSets)
-                {
-                    if (!dbSet.Type.IsAssignableTo(typeof(ISingleton)))
-                    {
-                        continue;
-                    }
-
-                    services.AddTransient(dbSet.Type, serviceProvider => ((IQueryable)serviceProvider.GetService<IContextCreator>().CreateFor(context.Type).GetDbSet(dbSet.Type)).Cast<object>().FirstOrDefault());
-                }
-            }
+            services.AddScoped<ISingletonGetter, SingletonGetter>();
         }
     }
 }
---
+++ Cloudy.CMS/SingletonSupport/SingletonGetter.cs
@@ -0,0 +1,18 @@
+﻿using Cloudy.CMS.ContextSupport;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cloudy.CMS.SingletonSupport
+{
+    public record SingletonGetter(IContextCreator ContextCreator) : ISingletonGetter
+    {
+        public async Task<T> Get<T>() where T : class
+        {
+            var context = ContextCreator.CreateFor(typeof(T));
+            var queryable = (IQueryable)context.GetDbSet(typeof(T));
+            return (T)await queryable.Cast<object>().FirstOrDefaultAsync().ConfigureAwait(false);
+        }
+    }
+}
