60e2e746ff71dc133a349be19b43cd1aaf6aba2e
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-12-20 20:05:48 +0100

    Update Vite setup

--- Cloudy.CMS.UI/wwwroot-src/vite.config.js
+++ Cloudy.CMS.UI/wwwroot-src/vite.config.js
@@ -1,31 +1,28 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
-import fs from 'fs';
-import alias from '@rollup/plugin-alias';
 
 export default defineConfig({
   plugins: [preact()],
+  resolve: {
+    alias: {
+      '@preact-htm': './preact-htm/standalone.module.js'
+    }
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  },
   build: {
     rollupOptions: {
       external: [
-        '/preact-htm/standalone.module.js'
+        './preact-htm/standalone.module.js'
       ],
       output: {
         entryFileNames: '[name].bundle.js',
         assetFileNames: '[name].[ext]',
         chunkFileNames: '[name].[ext]'
-      },
-      plugins: [
-        {
-          name: 'copy-files',
-          generateBundle: () => fs.copyFileSync('node_modules/htm/preact/standalone.module.js', '../wwwroot/preact-htm/standalone.module.js')
-        },
-        alias({
-          entries: [
-            { find: '@preact-htm', replacement: './preact-htm/standalone.module.js' }
-          ]
-        })
-      ]
+      }
     },
     sourcemap: true,
     outDir: '../wwwroot'
