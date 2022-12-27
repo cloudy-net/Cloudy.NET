c74aa47822822922002a350ff327728162f8407c
Björn Göransson <bjorn.a.goransson@gmail.com>
2022-12-27 18:32:20 +0100

    Add name

--- Cloudy.CMS.UI/wwwroot/Form/text.js
+++ Cloudy.CMS.UI/wwwroot/Form/text.js
@@ -2,7 +2,7 @@ import { html } from '/_content/Cloudy.CMS.UI/preact-htm/standalone.module.js';
 
 const Control = ({ name, value }) => {
     return html`<div>
-        <input type="text" class="form-control" id=${`field-${name}`} name="@Model.Field.Name" value=${value}/>
+        <input type="text" class="form-control" id=${`field-${name}`} name=${name} value=${value}/>
     </div>`;
 }
 
