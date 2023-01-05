84cab2663c3f47a7d830695088a28a16ea5e9480
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-05 16:37:03 +0100

    Split component

--- Cloudy.CMS.UI/wwwroot-src/form/form.jsx
+++ Cloudy.CMS.UI/wwwroot-src/form/form.jsx
@@ -2,60 +2,15 @@ import { useEffect, useState } from '@preact-htm';
 import FieldComponentProvider from './field-component-provider.jsx';
 import FormField from './form-field.jsx';
 import EntityContextProvider from './entity-context-provider'
+import FormFields from './form-fields.jsx';
 
 function Form({ contentType, keyValues }) {
-  const [loading, setLoading] = useState(true);
-  const [fields, setFields] = useState();
-  const [error, setError] = useState();
-  const [retryError, setRetryError] = useState(0);
-
-  useEffect(function () {
-    (async () => {
-      setError(null);
-
-      const response = await fetch(
-        `/Admin/api/form/fields?contentType=${contentType}`,
-        {
-          credentials: 'include'
-        }
-      );
-
-      if (!response.ok) {
-        setError({ response, body: await response.text() });
-        return;
-      }
-
-      var json = await response.json();
-
-      setLoading(false);
-      setFields(json);
-    })();
-  }, []);
-
-  let content;
-
-  if (error) {
-    content = <>
-      <div class="alert alert-primary">
-        <p>
-          There was an error (<code>{error.response.status}{error.response.statusText ? " " + error.response.statusText : ""}</code>)
-          loading your list{error.body ? ":" : "."}
-        </p>
-        {error.body ? <small><pre>{error.body}</pre></small> : ""}
-        <p class="mb-0"><button class="btn btn-primary" onClick={() => { setError(null); setTimeout(() => setRetryError(retryError + 1), 500); }}>Reload</button></p>
-      </div>
-    </>;
-  }
-
-  if (loading) {
-    return <>Loading ...</>;
-  }
-
-  return <EntityContextProvider {...{ contentType, keyValues }}>
-    <FieldComponentProvider>
-      {fields.map(field => <FormField {...field} path={[field.name]} />)}
-    </FieldComponentProvider>
-  </EntityContextProvider>
+  return <FieldComponentProvider>
+    <EntityContextProvider {...{ contentType, keyValues }}>
+      <FormFields {...{ contentType }} />
+      <button class="btn btn-primary" type='button'>Save</button>
+    </EntityContextProvider>
+  </FieldComponentProvider>
 };
 
 export default Form;
