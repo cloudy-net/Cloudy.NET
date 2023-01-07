51f1b7f0293e98565d15339a0d2f2497885fbb24
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-07 13:57:39 +0100

    Implement textarea

--- Cloudy.CMS.UI/wwwroot-src/form/controls/textarea.js
+++ Cloudy.CMS.UI/wwwroot-src/form/controls/textarea.js
@@ -1,3 +1,24 @@
-import { html } from '../../preact-htm/standalone.module.js';
+import { html, useContext } from '../../preact-htm/standalone.module.js';
+import stateManager from '../../data/state-manager.js';
+import EntityContext from '../entity-context.js';
+import getIntermediateSimpleValue from '../../util/get-intermediate-simple-value.js';
 
-export default () => html`<div>test</div>`;
+const Control = ({ name, path }) => {
+  const { contentReference, state } = useContext(EntityContext);
+
+  const onchange = event => {
+    stateManager.registerSimpleChange(contentReference, path, event.target.value)
+  };
+  return html`<div>
+      <textarea
+        type="text"
+        class="form-control"
+        id=${`field-${name}`}
+        name=${name}
+        defaultValue=${getIntermediateSimpleValue(state, path)}
+        onInput=${onchange}
+      />
+    </div>`;
+}
+
+export default Control;
