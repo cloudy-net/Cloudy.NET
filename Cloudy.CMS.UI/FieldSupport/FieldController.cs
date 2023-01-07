5225319f78da03ae691d26098f1a15a12691aa15
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-07 12:40:25 +0100

    Don't include primary keys in form

--- Cloudy.CMS.UI/FieldSupport/FieldController.cs
+++ Cloudy.CMS.UI/FieldSupport/FieldController.cs
@@ -1,4 +1,6 @@
 ﻿
+using Cloudy.CMS.ContentTypeSupport;
+using Cloudy.CMS.EntitySupport.PrimaryKey;
 using Cloudy.CMS.UI.FormSupport.FieldSupport;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,15 +14,17 @@ namespace Cloudy.CMS.UI.FieldSupport
 {
     [Authorize("adminarea")]
     [ResponseCache(NoStore = true)]
-    public record FieldController(IFieldProvider FieldProvider)
+    public record FieldController(IFieldProvider FieldProvider, IContentTypeProvider ContentTypeProvider, IPrimaryKeyPropertyGetter PrimaryKeyPropertyGetter)
     {
 
         [HttpGet]
         [Area("Admin")]
         [Route("/{area}/api/form/fields")]
-        public object GetFields(string contentType)
+        public object GetFields([FromQuery(Name = "contentType")] string contentTypeName)
         {
-            return FieldProvider.Get(contentType);
+            var contentType = ContentTypeProvider.Get(contentTypeName);
+            var primaryKeyProperties = PrimaryKeyPropertyGetter.GetFor(contentType.Type);
+            return FieldProvider.Get(contentTypeName).Where(f => !primaryKeyProperties.Any(p => p.Name == f.Name));
         }
     }
 }
