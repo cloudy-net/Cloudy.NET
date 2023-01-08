1e00cddf0afb736709714b6d9cac85cca670cbbc
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-08 11:44:58 +0100

    Fix HTML cursor stutter

--- Cloudy.CMS.UI/wwwroot-src/form/controls/html.js
+++ Cloudy.CMS.UI/wwwroot-src/form/controls/html.js
@@ -4,13 +4,13 @@ import EntityContext from '../entity-context.js';
 import getIntermediateSimpleValue from '../../util/get-intermediate-simple-value.js';
 
 
-document.addEventListener('click', function({ target }){ 
-    if(target.classList.contains('cloudy-ui-form-field-label') && target.nextSibling && target.nextSibling.classList.contains('quill-wrapper')){
-        const editor = target.nextSibling.querySelector('[contenteditable="true"]');
-        if(editor) {
-            editor.focus();
-        }
+document.addEventListener('click', function ({ target }) {
+  if (target.classList.contains('cloudy-ui-form-field-label') && target.nextSibling && target.nextSibling.classList.contains('quill-wrapper')) {
+    const editor = target.nextSibling.querySelector('[contenteditable="true"]');
+    if (editor) {
+      editor.focus();
     }
+  }
 });
 
 const Control = function ({ name, path }) {
@@ -19,33 +19,43 @@ const Control = function ({ name, path }) {
   const ref = useRef(null);
 
   useEffect(() => {
-      const instance = ref.current;
-      
-      if(!this.quill){
-          this.quill = new Quill(instance, {
-              theme: 'snow',
-              modules: {
-                  keyboard: {
-                      bindings: {
-                          tab: {
-                              key: 9,
-                              handler: () => true
-                          }
-                      }
-                  }
+    const instance = ref.current;
+
+    if (!this.quill) {
+      this.quill = new Quill(instance, {
+        theme: 'snow',
+        modules: {
+          keyboard: {
+            bindings: {
+              tab: {
+                key: 9,
+                handler: () => true
               }
-          });
-      }
-      
-      const callback = () => stateManager.registerSimpleChange(contentReference, path, this.quill.root.innerHTML.replace(/^\s*<p\s*>\s*<br\s*\/?>\s*<\/p\s*>\s*$/ig, ''));
+            }
+          }
+        }
+      });
+    }
 
-      this.quill.root.innerHTML = getIntermediateSimpleValue(state, path) || null;
+    const callback = () => {
+      const value = this.quill.root.innerHTML.replace(/^\s*<p\s*>\s*<br\s*\/?>\s*<\/p\s*>\s*$/ig, '');
+      this.quill.root.innerHTMLValue = value;
+      stateManager.registerSimpleChange(contentReference, path, value);
+    };
 
-      this.quill.on('text-change', callback);
+    this.quill.on('text-change', callback);
 
-      return () => {
-          this.quill.off('text-change', callback);
-      }
+    return () => {
+      this.quill.off('text-change', callback);
+    }
+  }, [contentReference]);
+
+  useEffect(() => {
+    const value = getIntermediateSimpleValue(state, path) || null;
+
+    if (this.quill.root.innerHTMLValue != value) {
+      this.quill.root.innerHTML = this.quill.root.innerHTMLValue = value;
+    }
   }, [state]);
 
   return html`
