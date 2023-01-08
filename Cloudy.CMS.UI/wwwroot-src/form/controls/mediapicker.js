6cfe7a250bf3f27d9ada487aa714c597aa943ea9
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-08 16:34:56 +0100

    Hook up mediapicker to state

--- Cloudy.CMS.UI/wwwroot-src/form/controls/mediapicker.js
+++ Cloudy.CMS.UI/wwwroot-src/form/controls/mediapicker.js
@@ -1,10 +1,14 @@
-import { useState, html } from '../../preact-htm/standalone.module.js';
+import { useState, html, useContext } from '../../preact-htm/standalone.module.js';
 import closeDropdown from "../../components/close-dropdown.js";
 import Dropdown from "../../components/dropdown.js";
 import MediaPickerMenu from "../../media-picker/media-picker-menu.js";
+import stateManager from '../../data/state-manager.js';
+import getIntermediateSimpleValue from '../../util/get-intermediate-simple-value.js';
+import EntityContext from '../entity-context.js';
 
-export default ({ controlName, provider, initialValue }) => {
-  const [value, setValue] = useState(initialValue);
+export default ({ name, path, provider }) => {
+  const { contentReference, state } = useContext(EntityContext);
+  const [value, setValue] = useState(getIntermediateSimpleValue(state, path));
 
   const copy = async () => {
     await navigator.clipboard.writeText(value);
@@ -16,15 +20,20 @@ export default ({ controlName, provider, initialValue }) => {
     setValue(text);
   };
 
+  const onchange = newValue => {
+    setValue(newValue != value ? newValue : null);
+    stateManager.registerSimpleChange(contentReference, path, newValue);
+  }
+
   return html`
-    <input type="hidden" class="form-control" name=${controlName} value=${value} />
+    <input type="hidden" class="form-control" name=${name} value=${value} />
 
     ${value && html`<div class="mb-2">
       <img class="media-picker-preview-image" src=${value} />
     </div>`}
 
     <${Dropdown} text="Add">
-      <${MediaPickerMenu} provider=${provider} value=${value} onSelect=${newValue => { setValue(newValue != value ? newValue : null); }} />
+      <${MediaPickerMenu} provider=${provider} value=${value} onSelect=${onchange} />
     <//>
 
     <${Dropdown} text="Other" className="ms-2">
