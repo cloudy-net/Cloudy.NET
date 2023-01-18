a2ba20084a44a8241a063228d39ffef9bea189fd
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-18 23:01:54 +0100

    Make tests more resilient

--- Cloudy.CMS.UI/wwwroot-src/test/simple-change-handler.test.js
+++ Cloudy.CMS.UI/wwwroot-src/test/simple-change-handler.test.js
@@ -19,12 +19,8 @@ describe('simple-change-handler.js', () => {
         }
       });
 
-      assert.equal(0, stateManager.getState(contentReference).changes.length);
       assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [propertyName]), initialValue, 'Initial value should be returned as there are no intermediate changes registered');
-
       simpleChangeHandler.setValue(stateManager, contentReference, [propertyName], newValue);
-
-      assert.equal(1, stateManager.getState(contentReference).changes.length);
       assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [propertyName]), newValue, 'New value should be returned as an intermediate change has been registered');
     });
     it('intermediate value, deep path', () => {
@@ -39,24 +35,24 @@ describe('simple-change-handler.js', () => {
         ...stateManager.getState(contentReference),
         referenceValues: {
           [blockName]: {
-            [block2Name]: {
-              [propertyName]: initialValue
+            Value: {
+              [block2Name]: {
+                Value: {
+                  [propertyName]: initialValue
+                }
+              }
             }
           }
         }
       });
 
-      assert.equal(0, stateManager.getState(contentReference).changes.length);
       assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), initialValue, 'Initial value should be returned as there are no intermediate changes registered');
-
       simpleChangeHandler.setValue(stateManager, contentReference, [blockName, block2Name, propertyName], newValue);
-
-      assert.equal(1, stateManager.getState(contentReference).changes.length);
       assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), newValue, 'New value should be returned as an intermediate change has been registered');
     });
   });
   describe('complex scenario', () => {
-    it('intermediate value should not be cleared when making a simple change after changing block type', () => {
+    it('simple change after changing block type', () => {
       const { contentReference } = stateManager.createStateForNewContent('page');
       const newType = 'BlockType';
       const blockName = 'Block';
@@ -68,18 +64,18 @@ describe('simple-change-handler.js', () => {
         ...stateManager.getState(contentReference),
         referenceValues: {
           [blockName]: {
-            [propertyName]: initialValue
+            Value: {
+              [propertyName]: initialValue
+            }
           }
         }
       });
 
       embeddedBlockChangeHandler.setType(stateManager, contentReference, [blockName], newType);
       simpleChangeHandler.setValue(stateManager, contentReference, [blockName, propertyName], newValue);
-
-      assert.equal(2, stateManager.getState(contentReference).changes.length, 'Number of registered changes should be 2');
-      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, propertyName]), newValue, 'New value expected');
+      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, propertyName]), newValue);
     });
-    it('intermediate value should be cleared when changing parent embedded block type', () => {
+    it('changing block type after simple change', () => {
       const { contentReference } = stateManager.createStateForNewContent('page');
       const newType = 'BlockType';
       const blockName = 'Block1';
@@ -92,18 +88,25 @@ describe('simple-change-handler.js', () => {
         ...stateManager.getState(contentReference),
         referenceValues: {
           [blockName]: {
-            [block2Name]: {
-              [propertyName]: initialValue
+            Value: {
+              [block2Name]: {
+                Value: {
+                  [propertyName]: initialValue
+                }
+              }
             }
           }
         }
       });
 
-      embeddedBlockChangeHandler.setType(stateManager, contentReference, [blockName, block2Name], newType);
       simpleChangeHandler.setValue(stateManager, contentReference, [blockName, block2Name, propertyName], newValue);
-
-      assert.equal(2, stateManager.getState(contentReference).changes.length, 'Number of registered changes should be 2');
-      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), newValue, 'New value expected');
+      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), newValue);
+      embeddedBlockChangeHandler.setType(stateManager, contentReference, [blockName, block2Name], newType);
+      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), null);
+      simpleChangeHandler.setValue(stateManager, contentReference, [blockName, block2Name, propertyName], newValue);
+      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), newValue);
+      embeddedBlockChangeHandler.setType(stateManager, contentReference, [blockName], newType);
+      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), null);
     });
     it('intermediate value should be cleared when changing parents parents embedded block type', () => {
       const { contentReference } = stateManager.createStateForNewContent('page');
@@ -118,8 +121,12 @@ describe('simple-change-handler.js', () => {
         ...stateManager.getState(contentReference),
         referenceValues: {
           [blockName]: {
-            [block2Name]: {
-              [propertyName]: initialValue
+            Value: {
+              [block2Name]: {
+                Value: {
+                  [propertyName]: initialValue
+                }
+              }
             }
           }
         }
@@ -127,9 +134,7 @@ describe('simple-change-handler.js', () => {
 
       simpleChangeHandler.setValue(stateManager, contentReference, [blockName, block2Name, propertyName], newValue);
       embeddedBlockChangeHandler.setType(stateManager, contentReference, [blockName, block2Name], newType);
-
-      assert.equal(2, stateManager.getState(contentReference).changes.length, 'Number of registered changes should be 2');
-      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), null, 'Cleared value expected');
+      assert.equal(simpleChangeHandler.getIntermediateValue(stateManager.getState(contentReference), [blockName, block2Name, propertyName]), null);
     });
   });
 });
