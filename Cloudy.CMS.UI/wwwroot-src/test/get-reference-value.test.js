d73d00c1d15aa55d7de06a99f768e7c5efcdfea7
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-18 22:39:48 +0100

    Add tests

---
+++ Cloudy.CMS.UI/wwwroot-src/test/get-reference-value.test.js
@@ -0,0 +1,55 @@
+import assert from 'assert';
+import arrayStartsWith from '../util/array-starts-with.js';
+import getReferenceValue from '../util/get-reference-value.js';
+
+describe('get-reference-value.js', () => {
+  it('simple property', async () => {
+    const propertyName = 'lorem';
+    const propertyValue = 'ipsum';
+
+    const state = {
+      referenceValues: {
+        [propertyName]: propertyValue
+      }
+    };
+    assert.equal(getReferenceValue(state, [propertyName]), propertyValue);
+  });
+  it('nested property', async () => {
+    const blockName = 'dolor';
+    const nestedBlockName = 'dolor';
+    const propertyName = 'lorem';
+    const propertyValue = 'ipsum';
+
+    const state = {
+      referenceValues: {
+        [blockName]: {
+          Value: {
+            [nestedBlockName]: {
+              Value: {
+                [propertyName]: propertyValue
+              }
+            }
+          }
+        }
+      }
+    };
+    assert.equal(getReferenceValue(state, [blockName, nestedBlockName, propertyName]), propertyValue);
+  });
+  it('nested property in null block', async () => {
+    const blockName = 'dolor';
+    const nestedBlockName = 'dolor';
+    const propertyName = 'lorem';
+    const propertyValue = null;
+
+    const state = {
+      referenceValues: {
+        [blockName]: {
+          Value: {
+            [nestedBlockName]: null
+          }
+        }
+      }
+    };
+    assert.equal(getReferenceValue(state, [blockName, nestedBlockName, propertyName]), propertyValue);
+  });
+});
