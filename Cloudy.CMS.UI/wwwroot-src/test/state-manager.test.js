895ed9bc8732ddc5ffc3fbd28016ed6c7ab0e748
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-18 12:58:46 +0100

    Add basic test

--- Cloudy.CMS.UI/wwwroot-src/test/state-manager.test.js
+++ Cloudy.CMS.UI/wwwroot-src/test/state-manager.test.js
@@ -1,9 +1,15 @@
 import assert from 'assert';
-import {} from './polyfiller.js';
+import { } from './polyfiller.js';
 import stateManager from '../data/state-manager.js';
 
 describe('state-manager.js', () => {
   describe('init', () => {
-    stateManager;
+    it('loads test reference entity', () => {
+      const entityType = 'page';
+      const keyValues = [1];
+      const contentReference = { entityType, keyValues, testReferenceValue: { hej: 1 } };
+      const state = stateManager.createOrUpdateStateForExistingContent(contentReference);
+      assert.strictEqual(state.referenceValues, contentReference.testReferenceValue)
+    });
   });
 });
