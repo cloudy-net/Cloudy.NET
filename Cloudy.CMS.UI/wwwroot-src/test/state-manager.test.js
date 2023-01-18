d60e13eb974f647ee5b32b30c7029970f88ea04d
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-18 14:58:10 +0100

    rename test

--- Cloudy.CMS.UI/wwwroot-src/test/state-manager.test.js
+++ Cloudy.CMS.UI/wwwroot-src/test/state-manager.test.js
@@ -4,7 +4,7 @@ import stateManager from '../data/state-manager.js';
 
 describe('state-manager.js', () => {
   describe('init', () => {
-    it('loads test reference entity', async () => {
+    it('create new state', async () => {
       const entityType = 'page';
       const state = await stateManager.createStateForNewContent(entityType);
       assert.ok(state);
