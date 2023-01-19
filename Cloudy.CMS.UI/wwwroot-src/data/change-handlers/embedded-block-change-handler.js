076075190aee0784cab674ddbebb0cd4c97af501
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-19 12:51:15 +0100

    Fix logic

--- Cloudy.CMS.UI/wwwroot-src/data/change-handlers/embedded-block-change-handler.js
+++ Cloudy.CMS.UI/wwwroot-src/data/change-handlers/embedded-block-change-handler.js
@@ -1,5 +1,8 @@
 import arrayEquals from "../../util/array-equals.js";
+import arrayStartsWith from "../../util/array-starts-with.js";
 import getReferenceValue from "../../util/get-reference-value.js";
+
+const UNCHANGED = {};
 
 class EmbeddedBlockChangeHandler {
   setType(stateManager, contentReference, path, type) {
@@ -12,14 +15,25 @@ class EmbeddedBlockChangeHandler {
     stateManager.persist(state);
   }
   getIntermediateType(state, path) {
-    const changes = state.changes.filter(c => c['$type'] == 'embeddedblock' && arrayEquals(c.path, path));
+    let type = UNCHANGED;
 
-    if (changes.length) {
-      return changes[changes.length - 1].type;
+    for (var change of state.changes) {
+      if (change['$type'] == 'embeddedblock' && arrayEquals(path, change.path)) {
+        type = change.type;
+        continue;
+      }
+      if (change['$type'] == 'embeddedblock' && arrayStartsWith(path, change.path)) {
+        type = null;
+        continue;
+      }
     }
 
-    const referenceValue = getReferenceValue(state, path);
-    return referenceValue ? referenceValue.Type : null;
+    if (type == UNCHANGED) {
+      const referenceValue = getReferenceValue(state, path);
+      return referenceValue ? referenceValue.Type : null;
+    }
+
+    return type;
   }
 }
 
