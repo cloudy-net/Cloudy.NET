b1bb66254aeb24b1e45153d2f3448a2116ddf591
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-22 17:58:51 +0100

    Find newly created states

--- Cloudy.CMS.UI/wwwroot-src/data/state-manager.js
+++ Cloudy.CMS.UI/wwwroot-src/data/state-manager.js
@@ -232,6 +232,12 @@ class StateManager {
   };
 
   getState(contentReference) {
+    if(contentReference.newContentKey && contentReference.keyValues){
+      contentReference = {
+        ...contentReference,
+        keyValues: null,
+      };
+    }
     return this.states.find(s => contentReferenceEquals(s.contentReference, contentReference));
   }
 
