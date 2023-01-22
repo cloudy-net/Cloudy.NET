5f89b35a51d2c9552d51c393b4b331fefc7f3632
jim-cloudy <jim-cloudy@ftwconsulting.se>
2023-01-22 11:49:26 +0100

    Verify value is persisted after reload

--- cypress.io/cypress/e2e/edit.cy.js
+++ cypress.io/cypress/e2e/edit.cy.js
@@ -6,6 +6,9 @@ describe('Edit test', () => {
     cy.get('input[name="Name"]').type('... cypress was here!');
     cy.get('button.btn.btn-primary').contains('Save').click()
     cy.get('input[name="Name"]').type(' - Yet again!');
-    cy.get('button.btn.btn-primary').contains('Save').click()
+    cy.get('button.btn.btn-primary').contains('Save').click();
+
+    cy.reload();
+    cy.get('input[name="Name"]').invoke('val').should('include', '... cypress was here! - Yet again!')
   })
 })
