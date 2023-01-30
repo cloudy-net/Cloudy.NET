60d2d65e4101db69d1d32293eb681f01f0f7cb20
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-01-30 11:47:04 +0100

    Hide changes if no merged changes exist

--- Cloudy.CMS.UI/wwwroot/form/changes.js
+++ Cloudy.CMS.UI/wwwroot/form/changes.js
@@ -4,7 +4,7 @@ import EntityContext from "./entity-context.js";
 
 
 const ChangedContentWarning = () => {
-  const { state } = useContext(EntityContext);
+  const { state, mergedChanges } = useContext(EntityContext);
   
   if(!state.newVersion){
     // return;
@@ -15,7 +15,7 @@ const ChangedContentWarning = () => {
   return html`
     ${
       true ?
-      html`<div style="text-align: right;"><a tabIndex="0" onClick=${() => setShowHistory(!showHistory)}>View changes</a></div>` :
+      html`<div style="text-align: right;">${mergedChanges.length ? html`<a tabIndex="0" onClick=${() => setShowHistory(!showHistory)}>View changes</a>` : html`<div style="white-space: pre"> <//>`}</div>` :
       html`<div class="alert alert-info">
         <strong>The source has changed since you started editing.</strong><br/>
         <a style="text-decoration: underline;" tabIndex="0" onClick=${() => setShowHistory(!showHistory)}>Review the changes</a> before you continue.
