fd29bc5affd1d05bca62f5d413234a34dad15cd7
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-02-09 22:57:47 +0100

    Add import

--- Cloudy.CMS.UI/wwwroot/data/state-manager.js
+++ Cloudy.CMS.UI/wwwroot/data/state-manager.js
@@ -3,6 +3,7 @@ import notificationManager from "../notification/notification-manager.js";
 import ContentNotFound from "./content-not-found.js";
 import statePersister from "./state-persister.js";
 import changeManager from "./change-manager.js";
+import conflictManager from "./conflict-manager.js";
 
 const generateRandomString = () => (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'); // https://stackoverflow.com/questions/5092808/how-do-i-randomly-generate-html-hex-color-codes-using-javascript
 const arrayEquals = (a, b) => {
