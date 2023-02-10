3f4f47fe9f98e4927026cce7dfd06b0c0ee4676f
jim-cloudy <jim-cloudy@ftwconsulting.se>
2023-02-10 16:24:10 +0100

    Await save requests

--- cypress.io/cypress/e2e/edit.cy.js
+++ cypress.io/cypress/e2e/edit.cy.js
@@ -1,13 +1,20 @@
 
 describe('Edit test', () => {
   it('Visit existing entity. Save twice.', () => {
+    cy.intercept('POST', '/Admin/api/form/entity/save').as('saving')
+
     cy.visit('/Admin/List?EntityType=Page');
     cy.get('table.table--content-list td a').first().click();
     cy.get('input[name="Name"]').clear().type('Hi!... cypress was here!');
+    
     cy.get('button.btn.btn-primary').contains('Save').click()
+    cy.wait('@saving')
+    
     cy.get('input[name="Name"]').type(' - Yet again!');
+
     cy.get('button.btn.btn-primary').contains('Save').click();
-    cy.wait(2000)
+    cy.wait('@saving')
+
     cy.reload();
     cy.get('input[name="Name"]').invoke('val').should('include', '... cypress was here! - Yet again!')
   })
