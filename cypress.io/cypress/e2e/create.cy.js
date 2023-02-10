51267c65ec321e6fc96a4081429e7ede1c113eae
jim-cloudy <jim-cloudy@ftwconsulting.se>
2023-02-10 17:21:06 +0100

    Cypress - Await save requests

--- cypress.io/cypress/e2e/create.cy.js
+++ cypress.io/cypress/e2e/create.cy.js
@@ -16,6 +16,9 @@ describe('Create test', () => {
 
   it('Create new entity. Save twice', () => {
 
+    // Add alias for save request
+    cy.intercept('POST', '/Admin/api/form/entity/save').as('saving')
+
     const uniqueName = `${Date.now()}_Cypress was here!`
     const modifiedUniqueName = `${uniqueName} - Yet again!`;
 
@@ -23,13 +26,17 @@ describe('Create test', () => {
 
     cy.get('input[name="Name"]').type(uniqueName);
     cy.get('button.btn.btn-primary').contains('Save').click();
-    cy.get('.alert.alert-warning', { timeout: 1000 }).should('not.exist');
+    cy.wait('@saving')
+    cy.wait(1000);
 
+    cy.get('.alert.alert-warning').should('not.exist');
 
     cy.get('input[name="Name"]').clear().type(modifiedUniqueName);
     cy.get('button.btn.btn-primary').contains('Save').click();
-    cy.get('.alert.alert-warning', { timeout: 1000 }).should('not.exist');
+    cy.wait('@saving')
+    cy.wait(1000);
 
+    cy.get('.alert.alert-warning').should('not.exist');
     cy.visit('/Admin/List?EntityType=Page');
     cy.get('.table-responsive ul.pagination a.page-link').not(':contains("Next")').last().click();
 
