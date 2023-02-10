5f74c330047cceb8bf2e1515fb99694c497d2d37
jim-cloudy <jim-cloudy@ftwconsulting.se>
2023-02-10 16:32:06 +0100

    Add delay before save

--- cypress.io/cypress/e2e/edit.cy.js
+++ cypress.io/cypress/e2e/edit.cy.js
@@ -1,20 +1,31 @@
 
 describe('Edit test', () => {
   it('Visit existing entity. Save twice.', () => {
+
+    // Add alias for save request
     cy.intercept('POST', '/Admin/api/form/entity/save').as('saving')
 
+    // Visist first page
     cy.visit('/Admin/List?EntityType=Page');
     cy.get('table.table--content-list td a').first().click();
-    cy.get('input[name="Name"]').clear().type('Hi!... cypress was here!');
     
+    // Modify name
+    cy.get('input[name="Name"]').clear().type('Hi!... cypress was here!');
+    cy.wait(100);
+
+    // Save and await request
     cy.get('button.btn.btn-primary').contains('Save').click()
     cy.wait('@saving')
     
+    // Modify name
     cy.get('input[name="Name"]').type(' - Yet again!');
+    cy.wait(100);
 
+    // Save and await request
     cy.get('button.btn.btn-primary').contains('Save').click();
     cy.wait('@saving')
 
+    // Reload and assert
     cy.reload();
     cy.get('input[name="Name"]').invoke('val').should('include', '... cypress was here! - Yet again!')
   })
