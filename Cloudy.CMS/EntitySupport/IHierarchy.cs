311488077a44140559dc68619ae12cc44a237007
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-02-16 22:15:42 +0100

    First stab at IHierarchy

---
+++ Cloudy.CMS/EntitySupport/IHierarchy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloudy.CMS.EntitySupport
+{
+    public interface IHierarchy<T>
+    {
+        T EntityId { get; set; }
+        T ParentId { get; set; }
+        IList<T> Ancestors { get; set; }
+        IList<T> Children { get; set; }
+    }
+}
