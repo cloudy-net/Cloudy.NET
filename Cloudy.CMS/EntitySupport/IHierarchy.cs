9252c2b936d734ed979fd13fcf36363a2f6ca574
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-02-18 17:20:42 +0100

    Update IHierarchy interface

--- Cloudy.CMS/EntitySupport/IHierarchy.cs
+++ Cloudy.CMS/EntitySupport/IHierarchy.cs
@@ -6,11 +6,17 @@ using System.Threading.Tasks;
 
 namespace Cloudy.CMS.EntitySupport
 {
-    public interface IHierarchy<T>
+    public interface IHierarchy<TKeyType>
     {
-        T EntityId { get; set; }
-        T ParentId { get; set; }
-        IList<T> Ancestors { get; set; }
-        IList<T> Children { get; set; }
+        public string? ParentType { get; set; }
+        public TKeyType? ParentId { get; set; }
+        public string EntityType { get; set; }
+        public TKeyType EntityId { get; set; }
+        public string? Name { get; set; }
+        public string? UrlPath { get; set; }
+        public int? SortIndex { get; set; }
+        public string? SortOrder { get; set; }
+        public IList<Tuple<string, TKeyType>> Ancestors { get; set; }
+        public IList<Tuple<string, TKeyType>> Children { get; set; }
     }
 }
---
+++ TestWebsite/Models/PageTree.cs
@@ -0,0 +1,20 @@
+﻿using Cloudy.CMS.EntitySupport;
+using System;
+using System.Collections.Generic;
+
+namespace TestWebsite.Models
+{
+    public class PageTree : IHierarchy<int>
+    {
+        public string? ParentType { get; set; }
+        public int? ParentId { get; set; }
+        public string EntityType { get; set; }
+        public int EntityId { get; set; }
+        public string? Name { get; set; }
+        public string? UrlPath { get; set; }
+        public int? SortIndex { get; set; }
+        public string? SortOrder { get; set; }
+        public IList<Tuple<string, int>> Ancestors { get; set; }
+        public IList<Tuple<string, int>> Children { get; set; }
+    }
+}
