e03bd0dfbe44bc8f2884eb0a224218a97a590d3e
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-02-18 17:29:46 +0100

    Remove nullable strings for now

--- Cloudy.CMS/EntitySupport/IHierarchy.cs
+++ Cloudy.CMS/EntitySupport/IHierarchy.cs
@@ -8,14 +8,14 @@ namespace Cloudy.CMS.EntitySupport
 {
     public interface IHierarchy<TKeyType>
     {
-        public string? ParentType { get; set; }
+        public string ParentType { get; set; }
         public TKeyType ParentId { get; set; }
         public string EntityType { get; set; }
         public TKeyType EntityId { get; set; }
-        public string? Name { get; set; }
-        public string? UrlPath { get; set; }
+        public string Name { get; set; }
+        public string UrlPath { get; set; }
         public int? SortIndex { get; set; }
-        public string? SortOrder { get; set; }
+        public string SortOrder { get; set; }
         public IList<Tuple<string, TKeyType>> Ancestors { get; set; }
         public IList<Tuple<string, TKeyType>> Children { get; set; }
     }
