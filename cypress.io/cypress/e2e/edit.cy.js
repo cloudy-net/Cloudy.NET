59b4f77115516a66c4405eec7044c28d1ba96c1c
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-02-21 19:27:40 +0100

    Add random waits

--- cypress.io/cypress/e2e/edit.cy.js
+++ cypress.io/cypress/e2e/edit.cy.js
@@ -4,14 +4,18 @@ describe('Page - Edit', () => {
 
     // Add spy for save request
     cy.saveSpy();
+    cy.wait(2000);
 
     // Visist first page
     cy.visit('/Admin/List?EntityType=Page');
+    cy.wait(2000);
     cy.get('table.table--content-list td a').first().click();
     
+    cy.wait(2000);
     // Modify name
     cy.typeName('Hi!... cypress was here!', true);
 
+    cy.wait(2000);
     // Save and await request
     cy.clickSave();
 
@@ -19,6 +23,7 @@ describe('Page - Edit', () => {
     
     // Modify name
     cy.typeName(' - Yet again!');
+    cy.wait(2000);
 
     // Save and await request
     cy.clickSave();
