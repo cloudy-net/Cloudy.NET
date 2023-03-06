c5c0550b5027e4dfd54e701fde0162b380e6b51f
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-03-06 05:53:42 +0100

    Delete old enum dropdown code

--- Cloudy.CMS.UI/wwwroot-src/enum-dropdown/enum-dropdown.jsx
+++
@@ -1,8 +0,0 @@
-export default ({ controlId, controlName, nullable, value, options }) => {
-  return <>
-    <select class="form-select" id={controlId} name={controlName} value={value}>
-      {nullable && <option value="">(empty)</option>}
-      {(options ? Object.entries(options) : []).map(([name, label]) => <option value={name}>{label}</option>)}
-    </select>
-  </>;
-};
