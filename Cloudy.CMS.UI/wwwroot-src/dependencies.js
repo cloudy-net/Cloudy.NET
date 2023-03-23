68cf24605fb8868128c77271740396d910de762b
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-03-23 10:34:52 +0100

    Rename to jsx

--- Cloudy.CMS.UI/wwwroot-src/dependencies.js
+++ Cloudy.CMS.UI/wwwroot-src/dependencies.js
@@ -5,7 +5,7 @@ import ValidationManager from './data/validation-manager.js';
 import urlFetcher from './util/url-fetcher.js';
 import simpleChangeHandler from './data/change-handlers/simple-change-handler.js';
 import blockTypeChangeHandler from './data/change-handlers/block-type-change-handler.js';
-import Dropdown from './components/dropdown.js';
+import Dropdown from './components/dropdown';
 import FormField from './form/form-field.js';
 import closeDropdown from './components/close-dropdown.js';
 import SearchBox from './components/search-box';
