45b717d2dd5923d3c8922bee4d1fabe88ddeea0d
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-03-23 11:34:36 +0100

    Start rendering placeholder for list

--- Cloudy.CMS.UI/wwwroot-src/form/form-field.jsx
+++ Cloudy.CMS.UI/wwwroot-src/form/form-field.jsx
@@ -4,7 +4,7 @@ import EntityContext from './contexts/entity-context';
 import FieldComponentContext from "./contexts/field-component-context";
 import ValidationManager from '../data/validation-manager.js';
 
-const FormField = ({ name, path, label, description, renderChrome, partial, settings, validators, dependencies }) => {
+const FormField = ({ name, path, label, description, renderChrome, partial, listPartial, settings, validators, dependencies }) => {
     const { componentContextProvider } = dependencies;
     const fieldComponents = useContext(FieldComponentContext);
 
@@ -12,15 +12,19 @@ const FormField = ({ name, path, label, description, renderChrome, partial, sett
         return;
     }
 
+    const control = listPartial ?
+        <>List</> :
+        html`<${fieldComponents[partial]} ...${{ name, label, path, settings, validators, dependencies }} />`;
+
     if (!renderChrome) {
-        return html`<${fieldComponents[partial]} ...${{ name, label, path, settings, validators, dependencies }} />`;
+        return control;
     }
 
     const { state } = useContext(EntityContext);
 
     return <div class={`mb-3 ${Object.keys(validators).length ? 'needs-validation' : ''} `}>
         <label for={componentContextProvider.getIdentifier(path)} class="form-label">{label} {state.changes.find(change => change.path == path) ? '*' : null}</label>
-        {html`<${fieldComponents[partial]} ...${{ name, label, path, settings, validators, dependencies }} />`}
+        {control}
         {description && <small class="form-text text-muted">{description}</small>}
         {Object.keys(validators).filter(v => ValidationManager.isInvalidForPathAndValidator(state.validationResults, path, v)).map(v =>
             <div class="invalid-feedback">{validators[v].message}</div>
