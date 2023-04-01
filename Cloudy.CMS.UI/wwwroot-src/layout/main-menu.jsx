14c4a99e17a6a01fbbc6421f9b4b500eeb63ea96
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-04-01 11:23:52 +0200

    Flip caret

--- Cloudy.CMS.UI/wwwroot-src/layout/main-menu.jsx
+++ Cloudy.CMS.UI/wwwroot-src/layout/main-menu.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "preact/hooks";
+import { useContext, useState } from "preact/hooks";
 import EntityTypesContext from "../form/contexts/entity-types-context";
 import { ReactComponent as DashboardIcon } from "../assets/icon-dashboard.svg";
 import { ReactComponent as EntityTypeIcon } from "../assets/icon-entity-type.svg";
@@ -8,6 +8,7 @@ import { Link } from "preact-router/match";
 
 const MainMenu = () => {
   const { groupedEntityTypes } = useContext(EntityTypesContext);
+  const [expanded, setExpanded] = useState({});
 
   return <div className="main-menu">
     <Cloud className="main-menu-cloud" />
@@ -17,14 +18,14 @@ const MainMenu = () => {
       </li>
       {Object.keys(groupedEntityTypes).sort().map(key => <>
         <li className="main-menu-item">
-          <a className="main-menu-item-button" tabIndex="0"><EntityTypeIcon className="main-menu-item-icon" /> {key} <Caret className="main-menu-item-caret" /></a>
-          <ul class="main-menu-sub-items">
+          <a className="main-menu-item-button" tabIndex="0" onClick={() => setExpanded({...expanded, [key]: !expanded[key]})}><EntityTypeIcon className="main-menu-item-icon" /> {key} <Caret className={"main-menu-item-caret" + (expanded[key] ? " expanded" : "")} /></a>
+          {expanded[key] && <ul class="main-menu-sub-items">
             {groupedEntityTypes[key].map(entityType =>
               <li className="main-menu-sub-item">
                 <Link activeClassName="active" class="main-menu-sub-item-button" href={`/Admin/List/${entityType.entityTypeName}`}>{entityType.pluralName}</Link>
               </li>
             )}
-          </ul>
+          </ul>}
         </li>
       </>)}
     </ul>
