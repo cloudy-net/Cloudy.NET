100fce621a19e4ce70e03a815477abad55e0ead3
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-04-17 21:36:12 +0200

    Start styling form

--- Cloudy.CMS.UI/wwwroot-src/form/form-field.jsx
+++ Cloudy.CMS.UI/wwwroot-src/form/form-field.jsx
@@ -23,8 +23,8 @@ const FormField = ({ name, path, label, description, renderChrome, partial, list
     const { state } = useContext(EntityContext);
 
     return <div class={`${path.indexOf(".") == -1 ? "root-" : ""}form-field ${Object.keys(validators).length ? 'needs-validation' : ''} `}>
-        <label for={componentContextProvider.getIdentifier(path)} class={`${path.indexOf(".") == -1 ? "root-" : ""}form-label`}>{label} {state.changes.find(change => change.path == path) ? '*' : null}</label>
-        {control}
+        <label class={`${path.indexOf(".") == -1 ? "root-" : ""}form-label`} for={componentContextProvider.getIdentifier(path)}>{label} {state.changes.find(change => change.path == path) ? '*' : null}</label>
+        <div class="form-control-outer">{control}</div>
         {description && <small class={`${path.indexOf(".") == -1 ? "root-" : ""}form-description`}>{description}</small>}
         {Object.keys(validators).filter(v => ValidationManager.isInvalidForPathAndValidator(state.validationResults, path, v)).map(v =>
             <div class={`${path.indexOf(".") == -1 ? "root-" : ""}form-validation-error`}>{validators[v].message}</div>
