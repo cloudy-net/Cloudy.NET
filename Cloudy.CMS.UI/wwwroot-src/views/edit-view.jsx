377545319fab165c63e79e85eaa3cb002e13b438
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-04-17 12:04:17 +0200

    Fix navbar title for edit mode

--- Cloudy.CMS.UI/wwwroot-src/views/edit-view.jsx
+++ Cloudy.CMS.UI/wwwroot-src/views/edit-view.jsx
@@ -2,14 +2,19 @@ import MainMenu from "../layout/main-menu";
 import Navbar from "../layout/navbar";
 import Form from "../form/form"
 import CompactList from "../entity-list/compact-list";
+import { useContext } from "preact/hooks";
+import EntityTypesContext from "../form/contexts/entity-types-context";
 
-const EditView = ({ entityTypeName, keyValues }) => <div class="layout show-navigation-panel">
-  <MainMenu />
-  <Navbar title="Edit" />
-  <CompactList entityType={entityTypeName} keyValues={keyValues} />
-  <div className="layout-main-panel">
-    <Form key={'form-edit'} mode="edit" entityTypeName={entityTypeName} keyValues={keyValues} />
-  </div>
-</div>;
+const EditView = ({ entityTypeName, keyValues }) => {
+  const { entityTypes } = useContext(EntityTypesContext);
+  return <div class="layout show-navigation-panel">
+    <MainMenu />
+    <Navbar title={entityTypes[entityTypeName] && entityTypes[entityTypeName].pluralName} />
+    <CompactList entityType={entityTypeName} keyValues={keyValues} />
+    <div className="layout-main-panel">
+      <Form key={'form-edit'} mode="edit" entityTypeName={entityTypeName} keyValues={keyValues} />
+    </div>
+  </div>;
+}
 
 export default EditView;
