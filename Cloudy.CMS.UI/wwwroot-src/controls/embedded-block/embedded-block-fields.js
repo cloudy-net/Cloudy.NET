5a34bb231f0f82c28f063c6e747759cd4a94eeb4
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-04-18 23:17:16 +0200

    Add menu kebab to embedded form

--- Cloudy.CMS.UI/wwwroot-src/controls/embedded-block/embedded-block-fields.js
+++ Cloudy.CMS.UI/wwwroot-src/controls/embedded-block/embedded-block-fields.js
@@ -1,52 +1,71 @@
 const EmbeddedBlockFields = ({ type, path, dependencies }) => {
-    const [loading, setLoading] = dependencies.useState(true);
-    const [fields, setFields] = dependencies.useState();
-    const [error, setError] = dependencies.useState();
-    const [retryError, setRetryError] = dependencies.useState(0);
-  
-    dependencies.useEffect(function () {
-      (async () => {
-        setError(null);
-  
-        const response = await fetch(
-          `/Admin/api/form/fields?type=${type}`,
-          {
-            credentials: 'include'
-          }
-        );
-  
-        if (!response.ok) {
-          setError({ response, body: await response.text() });
-          return;
+  const {
+    useState,
+    useEffect,
+    html,
+    FormField,
+    Dropdown,
+    DropdownItem,
+  } = dependencies;
+
+  const [loading, setLoading] = useState(true);
+  const [fields, setFields] = useState();
+  const [error, setError] = useState();
+  const [retryError, setRetryError] = useState(0);
+
+  useEffect(function () {
+    (async () => {
+      setError(null);
+
+      const response = await fetch(
+        `/Admin/api/form/fields?type=${type}`,
+        {
+          credentials: 'include'
         }
-  
-        var json = await response.json();
-  
-        setLoading(false);
-        setFields(json);
-      })();
-    }, []);
-  
-    let content;
-  
-    if (error) {
-      content = dependencies.html`
+      );
+
+      if (!response.ok) {
+        setError({ response, body: await response.text() });
+        return;
+      }
+
+      var json = await response.json();
+
+      setLoading(false);
+      setFields(json);
+    })();
+  }, []);
+
+  let content;
+
+  if (error) {
+    content = html`
         <div class="alert alert-primary">
           <p>
             There was an error (<code>${error.response.status}${error.response.statusText ? " " + error.response.statusText : ""}</code>)
             loading the block field types${error.body ? ":" : "."}
           </p>
-          ${error.body ? dependencies.html`<small><pre>{error.body}</pre></small>` : ""}
+          ${error.body ? html`<small><pre>{error.body}</pre></small>` : ""}
           <p class="mb-0"><button class="btn btn-primary" onClick=${() => { setError(null); setTimeout(() => setRetryError(retryError + 1), 500); }}>Reload</button></p>
         </div>
       `;
-    }
-  
-    if (loading) {
-      return dependencies.html`Loading ...`;
-    }
+  }
 
-    return dependencies.html`<fieldset class="embedded-block"><legend class="embedded-block-type">${type}<//>${fields.map(field => dependencies.html`<${dependencies.FormField} ...${field} path=${`${path}.${field.name}`} dependencies=${dependencies} />`)}<//>`;
+  if (loading) {
+    return html`Loading ...`;
+  }
+
+  const kebab = html`<svg class="embedded-block-type-kebab" width="18" height="4" viewBox="0 0 18 4" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="14" width="4" height="4" rx="2" fill="#ABB0BB"/><rect x="7" width="4" height="4" rx="2" fill="#ABB0BB"/><rect width="4" height="4" rx="2" fill="#ABB0BB"/></svg>`;
+
+  return html`<fieldset class="embedded-block">
+      <legend class="embedded-block-type">
+        ${type}
+        <${Dropdown} contents=${kebab} className="embedded-block-type-button">
+          <${DropdownItem} text="Remove" />
+        <//>
+      <//>
+      ${fields.map(field => html`<${FormField} ...${field} path=${`${path}.${field.name}`} dependencies=${dependencies} />`)}
+    <//>`;
 };
 
 export default EmbeddedBlockFields;
