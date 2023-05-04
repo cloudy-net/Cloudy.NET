6ce546293f362c92ef706eeaca7350af25a15d17
Björn Ali Göransson <bjorn.a.goransson@gmail.com>
2023-05-04 16:30:18 +0200

    Make entity context non-nullable

--- Cloudy.CMS.UI/wwwroot-src/src/form/contexts/entity-context.ts
+++ Cloudy.CMS.UI/wwwroot-src/src/form/contexts/entity-context.ts
@@ -4,7 +4,8 @@ import State from '../../data/state';
 import { Signal } from "@preact/signals-core";
 
 const EntityContext = createContext<{ entityReference: Signal<EntityReference | null>, state: Signal<State | null> }>({
-  
+  entityReference: new Signal<EntityReference | null>(null),
+  state: new Signal<State | null>(null),
 });
 
 export default EntityContext;
